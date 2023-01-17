054e78964f0c7d2e06d5638d8428d3a5a8f6f26c
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-17 15:40:43 +0100

    [FEAT] Added Component interface methods: update and receive

--- src/main/java/it/unibo/t2sgame/model/api/Component.java
+++ src/main/java/it/unibo/t2sgame/model/api/Component.java
@@ -1,5 +1,19 @@
 package it.unibo.t2sgame.model.api;
 
+/**
+ * This interface represents the components of the entities.
+ */
 public interface Component {
     
+    /**
+     * This method updates the domain of the component of specified entity.
+     * @param entity 
+     */
+    void update(Entity entity);
+
+    /**
+     * 
+     */
+    void receive();
+
 }
