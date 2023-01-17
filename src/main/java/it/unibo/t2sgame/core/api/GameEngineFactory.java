7bde305e3d1decea056933247df2250dead31e4c
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-17 15:23:53 +0100

    [FEAT] Added GameEngineFactory interface

---
+++ src/main/java/it/unibo/t2sgame/core/api/GameEngineFactory.java
@@ -0,0 +1,12 @@
+package it.unibo.t2sgame.core.api;
+/**
+ * A GameEngine AbstractFactory to produce GameEngine instance.
+ */
+public interface GameEngineFactory {
+    /**
+     * 
+     * @return an instance of GameEngine interface. The instance's sub-type depends
+     * on the current GameEngineFactory's implementation.
+     */
+    GameEngine createEngine();
+}
