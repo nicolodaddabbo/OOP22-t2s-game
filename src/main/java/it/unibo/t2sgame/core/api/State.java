240da6e95c8448b76d3dc36d5b1fb6c2d3c58e6f
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-17 16:02:32 +0100

    [FEAT] Added method in State Interface: isOver(), getRound(), incrementRound()

--- src/main/java/it/unibo/t2sgame/core/api/State.java
+++ src/main/java/it/unibo/t2sgame/core/api/State.java
@@ -1,5 +1,20 @@
 package it.unibo.t2sgame.core.api;
-
+/**
+ * Rapresent the current state of a Game
+ */
 public interface State {
-    
+    /**
+     * 
+     * @return true if logics determines the end of the game, otherwise false
+     */
+    boolean isOver();
+    /**
+     * 
+     * @return an integer rappresenting the number of the current round
+     */
+    int getRound();
+    /**
+     * Increment the number of round depends on the implementation
+     */
+    void incrementRound();
 }
