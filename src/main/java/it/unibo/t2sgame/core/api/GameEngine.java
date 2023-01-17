c58eecf27ab8a98166cd6102122bc8254111a71a
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-17 15:22:25 +0100

    [FEAT] Added run method in GameEngine interface

--- src/main/java/it/unibo/t2sgame/core/api/GameEngine.java
+++ src/main/java/it/unibo/t2sgame/core/api/GameEngine.java
@@ -1,5 +1,14 @@
 package it.unibo.t2sgame.core.api;
-
+/**
+ * This interface abstracts the concept of a "Game engine". 
+ * A game engine is the core of a game software architecture, which allows us
+ * to synchronize differents domain of the game, such as Input's handling with the 
+ * Physic's handling.
+ */
 public interface GameEngine {
-    
+    /**
+     * This method rapresent the game loop of the
+     * current GameEngine implementation.
+     */
+    void run();
 }
