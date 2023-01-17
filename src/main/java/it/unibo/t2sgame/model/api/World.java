b5a4ac0cb8983d9294b9667e949b7806575855ff
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-17 16:05:20 +0100

    [FEAT] Added method in World interface: getCurrentWave(),getPlayer()

--- src/main/java/it/unibo/t2sgame/model/api/World.java
+++ src/main/java/it/unibo/t2sgame/model/api/World.java
@@ -1,5 +1,17 @@
 package it.unibo.t2sgame.model.api;
-
+/**
+ * Rapresenting the "World" of T2S game.
+ * The World is like a "container" of different entities involved in the game.
+ */
 public interface World {
-    
+    /**
+     * 
+     * @return the wave of the current round 
+     */
+    Wave getCurrentWave();
+    /**
+     * 
+     * @return an entity rappresenting the "player" 
+     */
+    Entity getPlayer();
 }
