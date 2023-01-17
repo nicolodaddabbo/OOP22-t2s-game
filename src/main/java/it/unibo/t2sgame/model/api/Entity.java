ba61fd0728496ebf33b4ade0f4ede0ca4ac41d65
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-17 15:41:57 +0100

    [FEAT] Added Entity interface methods: getComponents, getGraphicComponent and notifyComponent

--- src/main/java/it/unibo/t2sgame/model/api/Entity.java
+++ src/main/java/it/unibo/t2sgame/model/api/Entity.java
@@ -1,5 +1,26 @@
 package it.unibo.t2sgame.model.api;
+import it.unibo.t2sgame.view.api.GraphicComponent;
+import java.util.List;
+import java.util.Optional;
 
+/**
+ * This interface represents the entities of the game.
+ */
 public interface Entity {
     
+    /**
+     * @return list of components of the entity
+     */
+    List<Component> getComponents();
+
+    /**
+     * @return optional of the GraphicComponent of the entity
+     */
+    Optional<GraphicComponent> getGraphicComponent();
+
+    /**
+     * 
+     */
+    void notifyComponent();
+
 }
