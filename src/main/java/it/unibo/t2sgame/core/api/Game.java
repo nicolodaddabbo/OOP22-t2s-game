55ebde243453681ed476c8ca993e38946712bd05
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-17 15:59:36 +0100

    [FEAT] Added method in Game interface: getState() getWorld()

--- src/main/java/it/unibo/t2sgame/core/api/Game.java
+++ src/main/java/it/unibo/t2sgame/core/api/Game.java
@@ -1,5 +1,18 @@
 package it.unibo.t2sgame.core.api;
 
+import it.unibo.t2sgame.model.api.World;
+/**
+ * This interface abstracts the concepts of "Game" ("Partita" in italian)
+ */
 public interface Game {
-    
+    /**
+     * 
+     * @return the state of the current game
+     */
+    State getState();
+    /**
+     * 
+     * @return the world of the current game
+     */
+    World getWorld();
 }
