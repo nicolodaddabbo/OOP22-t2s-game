a119e5f25ed8ddd3fceba3c78505ea8672b2599e
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-01-18 14:55:51 +0100

    [DOCS] Added javadoc comment to InputComponent interface

--- src/main/java/it/unibo/t2sgame/input/api/InputComponent.java
+++ src/main/java/it/unibo/t2sgame/input/api/InputComponent.java
@@ -2,6 +2,9 @@ package it.unibo.t2sgame.input.api;
 
 import it.unibo.t2sgame.model.api.Component;
 
+/**
+ * This interface represent the InputComponent of the entities
+ */
 public interface InputComponent extends Component {
     
 }
