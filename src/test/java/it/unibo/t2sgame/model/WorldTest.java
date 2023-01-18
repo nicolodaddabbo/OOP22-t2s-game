38c37a30772376469a7d2e95666240bc2da27459
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-18 21:48:03 +0100

    [TEST] WorldTest: Added new asserts in testBasicWorld

--- src/test/java/it/unibo/t2sgame/model/WorldTest.java
+++ src/test/java/it/unibo/t2sgame/model/WorldTest.java
@@ -1,25 +1,35 @@
 package it.unibo.t2sgame.model;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
 import it.unibo.t2sgame.input.api.InputComponent;
+import it.unibo.t2sgame.model.api.Entity;
+import it.unibo.t2sgame.model.api.Wave;
 import it.unibo.t2sgame.model.api.WorldFactory;
+import it.unibo.t2sgame.model.impl.EntityImpl;
 
 public class WorldTest {
     
     private WorldFactory factory = null;
     @Test
     void testBasicWorld(){
-        // Creating the world
         var world = this.factory.createBasicWorld();
-        // Check if getPlayer return an entity with an PlayerInputComponent
+        // Check if getPlayer return an entity with an InputComponent 
         assertTrue(world.getPlayers().get(0).getComponent(InputComponent.class).isPresent());
-        // Check if getCurrentWave returns a set of entity with an AIInputComponent
+        // Check if getCurrentWave returns a set of entity with an InputComponent
         assertTrue(world.getCurrentWave().getEnemies().stream().map(e -> e.getComponent(InputComponent.class).isPresent()).reduce((r1,r2) -> (r1 && r2)).get());
+        // Checking set a new Wave
+        Wave w = null;
+        world.setWave(w);
+        assertEquals(w,world.getCurrentWave());
+        // Checking add a new entity
+        Entity e = new EntityImpl();
+        world.addEntity(e);
+        assertTrue(world.getEntities().contains(e));
     }
 }
