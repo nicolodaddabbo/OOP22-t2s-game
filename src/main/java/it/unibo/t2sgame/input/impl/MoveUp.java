f535afdfbf96ef8056cd262ac343c7c363acd73e
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-01-18 16:44:35 +0100

    [FEAT] Added MoveUp.java

---
+++ src/main/java/it/unibo/t2sgame/input/impl/MoveUp.java
@@ -0,0 +1,13 @@
+package it.unibo.t2sgame.input.impl;
+
+import it.unibo.t2sgame.input.api.Command;
+import it.unibo.t2sgame.model.api.Entity;
+
+public class MoveUp implements Command {
+
+    @Override
+    public void execute(Entity entity) {
+        
+    }
+    
+}
