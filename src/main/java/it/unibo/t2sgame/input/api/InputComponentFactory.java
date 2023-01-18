6055abd66daa54b8dc04dc35777d7b2f9f5a1777
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-01-18 15:32:56 +0100

    [FEAT] Added InputComponentFactory

---
+++ src/main/java/it/unibo/t2sgame/input/api/InputComponentFactory.java
@@ -0,0 +1,16 @@
+package it.unibo.t2sgame.input.api;
+
+/**
+ * An InputComponent Factory to produce InputComponent instance.
+ */
+public interface InputComponentFactory {
+    /**
+     * @return an InputComponent that gets input from a Keyboard
+     */
+    InputComponent createKeyboardInputComponent();
+
+    /**
+     * @return an InputComponent that gets input from an AI
+     */
+    InputComponent createAIInputComponent();
+}
