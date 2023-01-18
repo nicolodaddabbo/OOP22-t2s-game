6f90923c49963047071b6a7d4b2da76aa263b519
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-18 14:28:40 +0100

    [FEAT] Added WorldFactoryImpl.java

---
+++ src/main/java/it/unibo/t2sgame/model/impl/WorldFactoryImpl.java
@@ -0,0 +1,14 @@
+package it.unibo.t2sgame.model.impl;
+
+import it.unibo.t2sgame.model.api.World;
+import it.unibo.t2sgame.model.api.WorldFactory;
+
+public class WorldFactoryImpl implements WorldFactory{
+
+    @Override
+    public World createBasicWorld() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
