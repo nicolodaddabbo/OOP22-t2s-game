c4544b71c9eab5c172b068d81894dea2003a6f59
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-01-18 09:51:24 +0100

    [FEAT] added GraphicComponent interface

--- src/main/java/it/unibo/t2sgame/view/api/GraphicComponent.java
+++ src/main/java/it/unibo/t2sgame/view/api/GraphicComponent.java
@@ -4,4 +4,9 @@ import it.unibo.t2sgame.model.api.Component;
 
 public interface GraphicComponent extends Component {
     
+    /**
+     * This method receives the graphic that the Entity is going to use
+     * @param graphic 
+     */
+    void setGraphics(Graphic graphic);
 }
