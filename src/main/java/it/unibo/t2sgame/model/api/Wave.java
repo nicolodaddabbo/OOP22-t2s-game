b63d695de6c834647ebd9dd9a316c3b77383a32a
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-01-18 10:11:35 +0100

    [FEAT] added Wave interface

--- src/main/java/it/unibo/t2sgame/model/api/Wave.java
+++ src/main/java/it/unibo/t2sgame/model/api/Wave.java
@@ -1,5 +1,12 @@
 package it.unibo.t2sgame.model.api;
 
+import java.util.List;
+
 public interface Wave {
     
+    /**
+     * 
+     * @return a list of Entity representing a Wave
+     */
+    List<Entity> getEnemies();
 }
