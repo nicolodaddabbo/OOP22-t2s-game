832d57e538ae878cf2b692e0d4a3fc96ed9013cd
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-18 14:30:02 +0100

    [REFACTOR] Deleted EntityImpl constructor

--- src/main/java/it/unibo/t2sgame/model/impl/EntityImpl.java
+++ src/main/java/it/unibo/t2sgame/model/impl/EntityImpl.java
@@ -14,13 +14,6 @@ public class EntityImpl implements Entity {
     private Optional<GraphicComponent> graphicComponent = Optional.empty();
     private Optional<InputComponent> inputComponent = Optional.empty();
     private Optional<PhysicComponent> physicComponent = Optional.empty();
-    
-    private EntityImpl(Entity entity){
-        this.components = entity.getComponents();
-        this.graphicComponent = entity.getGraphicComponent();
-        this.inputComponent = entity.getInputComponent();
-        this.physicComponent = entity.getPhysicComponent();
-    }
 
     @Override
     public Set<Component> getComponents() {
