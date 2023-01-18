e2190534e402b56f7b71ddb01ad07fc0db6727dc
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-01-18 09:44:51 +0100

    [FEAT] added Graphic interface

--- src/main/java/it/unibo/t2sgame/view/api/Graphic.java
+++ src/main/java/it/unibo/t2sgame/view/api/Graphic.java
@@ -1,5 +1,24 @@
 package it.unibo.t2sgame.view.api;
+import it.unibo.t2sgame.model.api.Entity;
 
 public interface Graphic {
     
+    /**
+     * This method draws a specified player Entity
+     * @param player 
+     */
+    void drawPlayer(Entity player);
+
+    /**
+     * This method draws a specified projectile Entity
+     * @param projectile 
+     */
+    void drawProjectile(Entity projectile);
+
+    /**
+     * This method draws a specified basic enemy Entity
+     * @param enemy 
+     */
+    void drawBaseEnemy(Entity enemy);
+
 }
