1736277eb529cfe1a4b097ef17780e91718e6a4c
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-19 13:15:47 +0100

    [FEAT] Added basic implementation of PhysicsComponent interface

---
+++ src/main/java/it/unibo/t2sgame/physics/impl/PhysicsComponentImpl.java
@@ -0,0 +1,36 @@
+package it.unibo.t2sgame.physics.impl;
+import it.unibo.t2sgame.common.Point2D;
+import it.unibo.t2sgame.common.Vector2D;
+import it.unibo.t2sgame.model.api.Entity;
+import it.unibo.t2sgame.physics.api.PhysicsComponent;
+
+public class PhysicsComponentImpl implements PhysicsComponent {
+
+    private Vector2D velocity;
+
+    /**
+     * Constructor to set starting velocity.
+     * @param velocity starting velocity
+     */
+    public PhysicsComponentImpl(Vector2D velocity){
+        this.velocity = velocity;
+    }
+
+    /**
+     * Updates the position of the passed entity if it has one.
+     * @param entity the entity to update
+     */
+    @Override
+    public void update(Entity entity) {
+        entity.getPosition().ifPresent(p -> {
+            Point2D position = p;
+            entity.setPosition(position.sum(velocity));
+        });
+    }
+
+    @Override
+    public void receive() {
+        
+    }
+    
+}
