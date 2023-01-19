cb5fe3e6dbe27bbe665148820c5405c7104785bd
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-19 09:04:07 +0100

    [TEST] WorldTest: Added testWorldWithOnePlayer

--- src/test/java/it/unibo/t2sgame/model/WorldTest.java
+++ src/test/java/it/unibo/t2sgame/model/WorldTest.java
@@ -1,35 +1,58 @@
 package it.unibo.t2sgame.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import it.unibo.t2sgame.input.api.InputComponent;
 import it.unibo.t2sgame.model.api.Entity;
+import it.unibo.t2sgame.model.api.EntityFactory;
 import it.unibo.t2sgame.model.api.Wave;
+import it.unibo.t2sgame.model.api.World;
 import it.unibo.t2sgame.model.api.WorldFactory;
 import it.unibo.t2sgame.model.impl.EntityImpl;
+import it.unibo.t2sgame.model.impl.WorldFactoryImpl;
 
 public class WorldTest {
     
-    private WorldFactory factory = null;
-    @Test
-    void testBasicWorld(){
-        var world = this.factory.createBasicWorld();
-        // Check if getPlayer return an entity with an InputComponent 
-        assertTrue(world.getPlayers().get(0).getComponent(InputComponent.class).isPresent());
-        // Check if getCurrentWave returns a set of entity with an InputComponent
-        assertTrue(world.getCurrentWave().getEnemies().stream().map(e -> e.getComponent(InputComponent.class).isPresent()).reduce((r1,r2) -> (r1 && r2)).get());
+    private WorldFactory worldFactory = new WorldFactoryImpl();
+    private EntityFactory entityFactory = null;
+
+    private void basics(final World w){
+         // Check if getPlayers return a list of entity with an Input Component
+         assertTrue(w.getPlayers().stream().map(p -> p.getComponent(InputComponent.class).isPresent()).reduce((r1,r2) -> (r1 && r2)).get());
+         // Check if getCurrentWave returns a set of entity with an InputComponent
+         assertTrue(w.getCurrentWave().getEnemies().stream().map(e -> e.getComponent(InputComponent.class).isPresent()).reduce((r1,r2) -> (r1 && r2)).get());
         // Checking set a new Wave
-        Wave w = null;
-        world.setWave(w);
-        assertEquals(w,world.getCurrentWave());
+        Wave wave = null;
+        w.setWave(wave);
+        assertEquals(wave,w.getCurrentWave());
         // Checking add a new entity
         Entity e = new EntityImpl();
-        world.addEntity(e);
-        assertTrue(world.getEntities().contains(e));
+        w.addEntity(e);
+        assertTrue(w.getEntities().contains(e));
+    }
+
+    @Test
+    void testWorldWithOnePlayer(){
+        var world = this.worldFactory.createWorldWithOnePlayer();
+        // Check if world contains only one player
+        assertTrue(world.getPlayers().size() == 1);
+        // Check the basics
+        basics(world);
+      
+    }
+
+    @Test
+    void testWorldWithMorePlayer(){
+        var players = List.of(this.entityFactory.createPlayer(), this.entityFactory.createPlayer());
+        var world = this.worldFactory.createWorldWithMorePlayer(players);
+        // Check if world contains more then one players
+        assertTrue(world.getPlayers().size() > 1);
+        // Check the basics
+        basics(world);
     }
 }
