b4f970681e4e099355c48fb7c206500a214e600a
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-19 19:49:59 +0100

    [FEAT] Added basic implementation of EntityFactory interface

---
+++ src/main/java/it/unibo/t2sgame/model/impl/EntityFactoryImpl.java
@@ -0,0 +1,27 @@
+package it.unibo.t2sgame.model.impl;
+
+import it.unibo.t2sgame.common.Vector2D;
+import it.unibo.t2sgame.input.impl.InputComponentFactoryImpl;
+import it.unibo.t2sgame.model.api.Entity;
+import it.unibo.t2sgame.model.api.EntityFactory;
+
+public class EntityFactoryImpl implements EntityFactory {
+
+    @Override
+    public Entity createPlayer() {
+        var entity = new EntityImpl();
+        entity.setPosition(new Vector2D(0, 0));
+        return entity.addComponent(new InputComponentFactoryImpl().createKeyboardInputComponent());
+    }
+
+    @Override
+    public Entity createProjectile() {
+        return null;
+    }
+
+    @Override
+    public Entity createBaseEnemy() {
+        return null;
+    }
+    
+}
