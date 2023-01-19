f8f4676ed1c8f455681449edbdda99dfab64ebb6
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-19 09:06:56 +0100

    [FEAT] Added StateImpl.java

---
+++ src/main/java/it/unibo/t2sgame/core/impl/StateImpl.java
@@ -0,0 +1,35 @@
+package it.unibo.t2sgame.core.impl;
+
+import java.util.List;
+
+import it.unibo.t2sgame.core.api.Game;
+import it.unibo.t2sgame.core.api.State;
+import it.unibo.t2sgame.model.api.Entity;
+import it.unibo.t2sgame.model.api.Wave;
+
+public class StateImpl implements State{
+    private int round = 0;
+
+    @Override
+    public boolean isOver(List<Entity> players) {
+        // Need to check the all players' life
+        return false;
+    }
+
+    @Override
+    public boolean isWaveOver(Wave w) {
+        return w.getEnemies().size() == 0;
+    }
+
+    @Override
+    public int getRound() {
+        return this.round;
+    }
+
+    @Override
+    public void incrementRound() {
+        this.round++;        
+    }
+
+    
+}
