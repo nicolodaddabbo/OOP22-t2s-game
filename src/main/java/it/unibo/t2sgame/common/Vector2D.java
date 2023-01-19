d4649201343b7b1316605527cc4fef5bdf3c4794
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-19 15:43:44 +0100

    [FEAT] Added method to Vector2D: hashCode(), equals() and toString()

--- src/main/java/it/unibo/t2sgame/common/Vector2D.java
+++ src/main/java/it/unibo/t2sgame/common/Vector2D.java
@@ -78,4 +78,37 @@ public class Vector2D {
         return Math.sqrt(Math.pow(point.x-this.x, 2)+Math.pow(point.y-this.y, 2));
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vector2D other = (Vector2D) obj;
+        if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+            return false;
+        if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector2D [x=" + x + ", y=" + y + "]";
+    }
+
 }
