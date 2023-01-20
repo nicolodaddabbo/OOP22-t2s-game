e99100023c0c17b070972269a2dae004b2554cb2
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-01-20 15:14:50 +0100

    [REFACTOR] added setter methods to interface GameScene: setGame and setKeyboardInputController

--- src/main/java/it/unibo/t2sgame/view/api/GameScene.java
+++ src/main/java/it/unibo/t2sgame/view/api/GameScene.java
@@ -1,9 +1,28 @@
 package it.unibo.t2sgame.view.api;
 
+import it.unibo.t2sgame.core.api.Game;
+import it.unibo.t2sgame.input.impl.KeyboardInputController;
+/**
+ * Interface representing a GameScene of the game 
+ */
 public interface GameScene {
-    
+    /**
+     * method that render the GameScene using a Graphic technology
+     */
     void render();
-    
+    /**
+     * method that initialize the GameScene
+     */
     void initialize();
-    
+    /**
+     * 
+     * @param game representing the game that is going to be played and rendered
+     */
+    void setGame(Game game);
+    /**
+     * 
+     * @param keyInController representing the input controller that uses the key clicked by the 
+     *                        user on a keyboard
+     */
+    void setKeyboardInputController(KeyboardInputController keyInController);
 }
