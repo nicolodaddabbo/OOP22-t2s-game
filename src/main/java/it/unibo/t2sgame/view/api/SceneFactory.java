924b94b6b25a6e8ae91c239cb0b712aab9a33320
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-01-20 14:39:31 +0100

    [REFACTOR] changed method name in SceneFactory from createScene to createJavaFXScene()

--- src/main/java/it/unibo/t2sgame/view/api/SceneFactory.java
+++ src/main/java/it/unibo/t2sgame/view/api/SceneFactory.java
@@ -5,11 +5,5 @@ public interface SceneFactory {
      * 
      * @return a created Scene
      */
-    GameScene createScene();
-
-    /**
-     * 
-     * @return the Graphic that the application is going to use
-     */
-    Graphic createGraphic();
+    GameScene createJavaFXScene();
 }
