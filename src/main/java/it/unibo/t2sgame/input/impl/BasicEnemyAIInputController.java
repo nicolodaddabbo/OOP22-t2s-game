90384d8dfd9284f3daff8b807df44ffab7f14622
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-01-20 15:59:32 +0100

    [REFACTOR] Used just a Move class instead of MoveUp, MoveDown, MoveLeft, MoveRight

--- src/main/java/it/unibo/t2sgame/input/impl/BasicEnemyAIInputController.java
+++ src/main/java/it/unibo/t2sgame/input/impl/BasicEnemyAIInputController.java
@@ -1,18 +1,27 @@
 package it.unibo.t2sgame.input.impl;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
 import it.unibo.t2sgame.input.api.Command;
+import it.unibo.t2sgame.input.api.Directions;
 import it.unibo.t2sgame.input.api.InputController;
 
 public class BasicEnemyAIInputController implements InputController {
-    final List<Command> availableCommands = List.of(new MoveUp(), new MoveDown(), new MoveRight(), new MoveLeft());
+    private final List<Command> availableCommands;
+    private final Random random = new Random();
+
+    public BasicEnemyAIInputController() {
+        this.availableCommands = new LinkedList<>();
+        for (var direction : Directions.values()) {
+            this.availableCommands.add(new Move(direction));
+        }
+    }
 
     @Override
     public Optional<Command> getCommand() {
-        final Random random = new Random();
         return Optional.of(availableCommands.get(random.nextInt(availableCommands.size())));
     }
     
