d76210e1e2df251d4b4bc083acccd3b124553371
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-20 13:49:24 +0100

    [REFACTOR] Modified methods of EntityFactoryImpl

--- src/main/java/it/unibo/t2sgame/model/impl/EntityFactoryImpl.java
+++ src/main/java/it/unibo/t2sgame/model/impl/EntityFactoryImpl.java
@@ -4,22 +4,31 @@ import it.unibo.t2sgame.common.Vector2D;
 import it.unibo.t2sgame.input.impl.InputComponentFactoryImpl;
 import it.unibo.t2sgame.model.api.Entity;
 import it.unibo.t2sgame.model.api.EntityFactory;
+import it.unibo.t2sgame.view.impl.GraphicComponentFactoryImpl;
 
 public class EntityFactoryImpl implements EntityFactory {
 
     @Override
     public Entity createPlayer(final Vector2D position) {
-        return new EntityImpl(position).addComponent(new InputComponentFactoryImpl().createKeyboardInputComponent());
+        return new EntityImpl(position)
+            .addComponent(new InputComponentFactoryImpl().createKeyboardInputComponent())
+            .addComponent(null)
+            .addComponent(new GraphicComponentFactoryImpl().getPlayerGraphicComponent());
     }
 
     @Override
     public Entity createProjectile(final Vector2D position) {
-        return null;
+        return new EntityImpl(position)
+            .addComponent(null)
+            .addComponent(new GraphicComponentFactoryImpl().getProjectileGraphicComponent());
     }
 
     @Override
     public Entity createBaseEnemy(final Vector2D position) {
-        return new EntityImpl(position).addComponent(new InputComponentFactoryImpl().createKeyboardInputComponent());
+        return new EntityImpl(position)
+            .addComponent(new InputComponentFactoryImpl().createBasicEnemyAIInputComponent())
+            .addComponent(null)
+            .addComponent(new GraphicComponentFactoryImpl().getBaseEnemyGraphicComponent());
     }
     
 }
