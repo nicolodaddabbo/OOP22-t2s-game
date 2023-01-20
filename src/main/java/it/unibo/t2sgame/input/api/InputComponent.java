4865a22dc477b80dd6d0af36a2a00cf1ce900cc7
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-01-20 15:44:36 +0100

    [FEAT] Added getInputController in InputComponent interface

--- src/main/java/it/unibo/t2sgame/input/api/InputComponent.java
+++ src/main/java/it/unibo/t2sgame/input/api/InputComponent.java
@@ -6,5 +6,5 @@ import it.unibo.t2sgame.model.api.Component;
  * This interface represent the InputComponent of the entities
  */
 public interface InputComponent extends Component {
-    
+    InputController getInputController();
 }
