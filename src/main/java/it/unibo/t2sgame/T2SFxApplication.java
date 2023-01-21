f9acde8865c6e35ef2eb3eb1a1a35b6921be6ae3
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-21 09:03:34 +0100

    [FEAT] Added JafaFx Laucher and Application starter

---
+++ src/main/java/it/unibo/t2sgame/T2SFxApplication.java
@@ -0,0 +1,26 @@
+package it.unibo.t2sgame;
+
+import it.unibo.t2sgame.core.impl.GameEngineFactoryImpl;
+import it.unibo.t2sgame.core.impl.GameFactoryImpl;
+import it.unibo.t2sgame.view.impl.SceneFactoryImpl;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class T2SFxApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        var scene = new SceneFactoryImpl().createJavaFXScene(); 
+        var engine = new GameEngineFactoryImpl().createGameEngineWithFpsLock(scene);
+        // Needing to set the observer view
+        //scene.setObserver(engine);
+        scene.initialize();
+        /*
+         * Letting the JavaFx Thread to be free in order to handle the GUI.
+         * Not creating a new thread to handle game loop would JavaFx Thread
+         * to be used in the loop causing the crash of the GUI 
+         */
+        new Thread(() -> engine.setGame(new GameFactoryImpl().createSinglePlayerGame()).run());
+    }
+    
+}
---
+++ src/main/java/it/unibo/t2sgame/T2SFxLauncher.java
@@ -0,0 +1,9 @@
+package it.unibo.t2sgame;
+
+import javafx.application.Application;
+
+public class T2SFxLauncher {
+    public static void main(final String... args) {
+        Application.launch(T2SFxApplication.class, args);
+    }
+}
