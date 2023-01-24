91a278fb8370f155bd70288f3c634aa2cede98ed
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-24 20:51:58 +0100

    [DOCS] Added comments to diffents fields and update method

--- src/main/java/it/unibo/t2sgame/core/api/GameEngine.java
+++ src/main/java/it/unibo/t2sgame/core/api/GameEngine.java
@@ -9,18 +9,56 @@ import it.unibo.t2sgame.model.api.Entity;
 import it.unibo.t2sgame.view.api.GameScene;
 
 public interface GameEngine {
-    
+    /**
+     * Update all the systems contained in the engine.
+     * This method has to be called once per game loop's 
+     * cycle
+     */
     void update(); 
-    
+    /**
+     * Get the entities contained in the engine
+     * 
+     * @return the entities contained in the engine
+     */
     List<Entity> getEntities();
-
+    /**
+     * Add an entity to the engine.
+     * Once added, this entity is automatically associated with its
+     * related GameSystems, based on the component that it has.
+     * 
+     * @param e the entity to be added to the engine
+     * 
+     * @return this
+     */
     GameEngine addEntity(Entity e);
-
+    /**
+     * Remove the entity from the engine.
+     * Once removed, this entity is automatically removed from its
+     * related GameSystems.
+     * @param e
+     * @return
+     */
     GameEngine removeEntity(Entity e);
-
+    /**
+     * Get the GameSystems added to the engine
+     * 
+     * @return the GameSystems added to the engine
+     */
     Set<GameSystem> getSystems();
-
+    /**
+     * Get the GameSystem based on the type of component which it 
+     * contains.
+     * 
+     * @param <T> The type of the component 
+     * @param clazz the class instance of the component
+     * 
+     * @return an Optional containing the GameSystem if present, 
+     * otherwise an empty Optional
+     */
     <T extends Component> Optional<GameSystem> getSystem(Class<T> clazz); 
-
+    /**
+     * Set the scene to be rendered by the Graphics System
+     * @param scene
+     */
     void setScene(GameScene scene);    
 }
