25a259e767dd558aaa9a8325fe3d5e15e55cd413
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-24 15:30:39 +0100

    [REFACTOR] Refactored the entry point of the game by entering with game.start() method

--- src/main/java/it/unibo/t2sgame/T2SFxApplication.java
+++ src/main/java/it/unibo/t2sgame/T2SFxApplication.java
@@ -1,28 +1,37 @@
 package it.unibo.t2sgame;
 
 import it.unibo.t2sgame.core.api.Game;
-import it.unibo.t2sgame.core.impl.GameEngineFactoryImpl;
 import it.unibo.t2sgame.core.impl.GameFactoryImpl;
-import it.unibo.t2sgame.input.impl.KeyboardInputController;
 import it.unibo.t2sgame.view.impl.SceneFactoryImpl;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class T2SFxApplication extends Application {
 
+ 
+    private Game game = new GameFactoryImpl().createSinglePlayerGame();
     @Override
     public void start(Stage primaryStage) throws Exception {
         var scene = new SceneFactoryImpl().createJavaFXScene(); 
-        var engine = new GameEngineFactoryImpl().createGameEngineWithFpsLock(scene);
+        game.setScene(scene);
         // Needing to set the observer view
-        scene.setEngine(engine);
         scene.initialize();
         /*
          * Letting the JavaFx Thread to be free in order to handle the GUI.
          * Not creating a new thread to handle game loop would JavaFx Thread
          * to be used in the loop causing the crash of the GUI 
          */
-        new Thread(() -> engine.setGame(new GameFactoryImpl().createSinglePlayerGame()).run()).start();
+        new Thread(() -> this.run(new GameFactoryImpl().createSinglePlayerGame())).start();
+    }
+
+
+    public void run(Game game){
+        // Initialize the game initializing scene settings
+        this.game.initSettings();
+        // Initialize the game initializing entities
+        this.game.initGame();
+        // Starting the game
+        this.game.start();
     }
     
 }
