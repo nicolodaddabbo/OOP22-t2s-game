472c0d725de630ed9cf79e1f2460f1db646cbacc
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-01-26 09:13:33 +0100

    [REFACTOR] Moved GameImpl from the factory to a class

--- src/main/java/it/unibo/t2sgame/core/impl/GameFactoryImpl.java
+++ src/main/java/it/unibo/t2sgame/core/impl/GameFactoryImpl.java
@@ -1,108 +1,30 @@
 package it.unibo.t2sgame.core.impl;
 
 import java.util.List;
-import java.util.Optional;
 
 import it.unibo.t2sgame.common.Vector2D;
-import it.unibo.t2sgame.core.api.GameEngine;
 import it.unibo.t2sgame.core.api.Game;
 import it.unibo.t2sgame.core.api.GameFactory;
-import it.unibo.t2sgame.core.api.State;
 import it.unibo.t2sgame.model.api.EntityFactory;
-import it.unibo.t2sgame.model.api.World;
 import it.unibo.t2sgame.model.api.WorldFactory;
 import it.unibo.t2sgame.model.impl.EntityFactoryImpl;
-import it.unibo.t2sgame.model.impl.WaveFactoryImpl;
 import it.unibo.t2sgame.model.impl.WorldFactoryImpl;
-import it.unibo.t2sgame.view.api.GameScene;
 
 public class GameFactoryImpl implements GameFactory {
-
-
-    private Game gameFrom(final State s, final World w){
-        return new Game() {
-            private Optional<GameScene> scene = Optional.empty();
-
-            private GameEngine engine = new GameEngineImpl();
-
-            @Override
-            public State getState() {
-                return s;
-            }
-
-            @Override
-            public World getWorld() {
-                return w;
-            }
-
-            @Override
-            public void nextWave() {
-                s.incrementRound();
-                var wave = new WaveFactoryImpl().createBasicWave(s.getRound());
-                wave.getEnemies().forEach(e -> {
-                    e.setWorld(w);
-                    this.engine.addEntity(e);
-                });
-                w.setWave(wave);                
-            }
-
-            @Override
-            public void start() {
-                while(!this.isOver()){
-                    this.addWaveIfOver();
-                    engine.update();
-                }
-              
-            }
-
-            @Override
-            public void setScene(GameScene scene) {
-                this.scene = Optional.of(scene);                
-            }
-            
-
-            private void addWaveIfOver(){
-                if(s.isWaveOver(w.getCurrentWave())){
-                    this.nextWave();
-                }
-            }
-
-            @Override
-            public boolean isOver() {
-                return s.isOver(w.getPlayers());
-            }
-
-            @Override
-            public void initSettings() {
-                this.scene.ifPresent(scene -> scene.setEngine(engine));
-                this.scene.ifPresent(scene -> scene.setGame(this));
-                engine.setScene(this.scene.get());
-            }
-
-            @Override
-            public void initGame() {
-                w.getEntities().forEach(e -> this.engine.addEntity(e));
-            }
-            
-            
-        };
-    }
-
-    private EntityFactory entityFactory = new EntityFactoryImpl();
-    private WorldFactory worldFactory = new WorldFactoryImpl();
+    private final EntityFactory entityFactory = new EntityFactoryImpl();
+    private final WorldFactory worldFactory = new WorldFactoryImpl();
 
     @Override
     public Game createSinglePlayerGame() {
-        return gameFrom(new StateImpl(), this.worldFactory.createWorldWithOnePlayer());
+        return new GameImpl(new StateImpl(), this.worldFactory.createWorldWithOnePlayer());
     }
 
     @Override
     public Game createMultiPlayerGame() {
-        return gameFrom(new StateImpl(),
-        this.worldFactory.createWorldWithMorePlayer(
-            List.of(
-                this.entityFactory.createPlayer(new Vector2D(0, 0)),
-                this.entityFactory.createPlayer(new Vector2D(0, 0)))));
+        return new GameImpl(new StateImpl(), this.worldFactory.createWorldWithMorePlayer(
+                List.of(
+                        this.entityFactory.createPlayer(new Vector2D(0, 0)),
+                        this.entityFactory.createPlayer(new Vector2D(0, 0)))));
     }
-    
+
 }
---
+++ src/main/java/it/unibo/t2sgame/core/impl/GameImpl.java
@@ -0,0 +1,81 @@
+package it.unibo.t2sgame.core.impl;
+
+import java.util.Optional;
+
+import it.unibo.t2sgame.core.api.Game;
+import it.unibo.t2sgame.core.api.GameEngine;
+import it.unibo.t2sgame.core.api.State;
+import it.unibo.t2sgame.model.api.World;
+import it.unibo.t2sgame.model.impl.WaveFactoryImpl;
+import it.unibo.t2sgame.view.api.GameScene;
+
+public class GameImpl implements Game {
+    private Optional<GameScene> scene = Optional.empty();
+    private final GameEngine engine = new GameEngineImpl();
+    private final State state;
+    private final World world;
+
+    public GameImpl(final State state, final World world) {
+        this.state = state;
+        this.world = world;
+    }
+
+    @Override
+    public State getState() {
+        return this.state;
+    }
+
+    @Override
+    public World getWorld() {
+        return this.world;
+    }
+
+    @Override
+    public void nextWave() {
+        this.state.incrementRound();
+        var wave = new WaveFactoryImpl().createBasicWave(this.state.getRound());
+        wave.getEnemies().forEach(e -> {
+            e.setWorld(world);
+            this.engine.addEntity(e);
+        });
+        this.world.setWave(wave);
+    }
+
+    @Override
+    public void start() {
+        while (!this.isOver()) {
+            this.addWaveIfOver();
+            this.engine.update();
+        }
+
+    }
+
+    @Override
+    public void setScene(GameScene scene) {
+        this.scene = Optional.of(scene);
+    }
+
+    private void addWaveIfOver() {
+        if (this.state.isWaveOver(this.world.getCurrentWave())) {
+            this.nextWave();
+        }
+    }
+
+    @Override
+    public boolean isOver() {
+        return this.state.isOver(this.world.getPlayers());
+    }
+
+    @Override
+    public void initSettings() {
+        this.scene.ifPresent(s -> s.setEngine(this.engine));
+        this.scene.ifPresent(s -> s.setGame(this));
+        this.engine.setScene(this.scene.get());
+    }
+
+    @Override
+    public void initGame() {
+        this.world.getEntities().forEach(this.engine::addEntity);
+    }
+
+}
