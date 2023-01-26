2361cabe59cdf7fa0e3ad07b352f17b090c8c90e
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-01-26 14:28:36 +0100

    [FEAT] Added Keyboard Input support for shooting

--- src/main/java/it/unibo/t2sgame/input/impl/KeyboardInputController.java
+++ src/main/java/it/unibo/t2sgame/input/impl/KeyboardInputController.java
@@ -12,12 +12,20 @@ public class KeyboardInputController implements InputController {
     private static final int MOVE_RIGHT_CODE = 68;
     private static final int MOVE_DOWN_CODE = 83;
     private static final int MOVE_LEFT_CODE = 65;
+    private static final int SHOOT_UP_CODE = 38;
+    private static final int SHOOT_RIGHT_CODE = 39;
+    private static final int SHOOT_DOWN_CODE = 40;
+    private static final int SHOOT_LEFT_CODE = 37;
 
     private static final Map<Integer, Command> MOVESET = Map.of(
         MOVE_UP_CODE, new Move(Directions.UP),
         MOVE_RIGHT_CODE, new Move(Directions.RIGHT),
         MOVE_DOWN_CODE, new Move(Directions.DOWN),
-        MOVE_LEFT_CODE, new Move(Directions.LEFT)
+        MOVE_LEFT_CODE, new Move(Directions.LEFT),
+        SHOOT_UP_CODE, new Shoot(Directions.UP),
+        SHOOT_RIGHT_CODE, new Shoot(Directions.RIGHT),
+        SHOOT_DOWN_CODE, new Shoot(Directions.DOWN),
+        SHOOT_LEFT_CODE, new Shoot(Directions.LEFT)
     );
 
     private Optional<Command> command = Optional.empty();
---
+++ src/main/java/it/unibo/t2sgame/input/impl/Shoot.java
@@ -0,0 +1,19 @@
+package it.unibo.t2sgame.input.impl;
+
+import it.unibo.t2sgame.core.components.api.ShootComponent;
+import it.unibo.t2sgame.core.entity.api.Entity;
+import it.unibo.t2sgame.input.api.Command;
+import it.unibo.t2sgame.input.api.Directions;
+
+public class Shoot implements Command {
+    private final Directions direction;
+
+    public Shoot(final Directions direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public void execute(final Entity entity) {
+        entity.notifyComponent(ShootComponent.class, () -> this.direction);
+    }
+}
