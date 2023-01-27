5d383386670b9f2cc4596504834b86ed3601a28f
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-01-27 15:47:36 +0100

    [FIX] EventFactoryImpl implements EventFactory

--- src/main/java/it/unibo/t2sgame/game/logics/impl/EventFactoryImpl.java
+++ src/main/java/it/unibo/t2sgame/game/logics/impl/EventFactoryImpl.java
@@ -2,11 +2,12 @@ package it.unibo.t2sgame.game.logics.impl;
 
 import it.unibo.t2sgame.core.entity.api.Entity;
 import it.unibo.t2sgame.game.logics.api.Event;
+import it.unibo.t2sgame.game.logics.api.EventFactory;
 import it.unibo.t2sgame.game.model.api.EntityFactory;
 import it.unibo.t2sgame.game.model.impl.EntityFactoryImpl;
 import it.unibo.t2sgame.input.api.Directions;
 
-public class EventFactoryImpl {
+public class EventFactoryImpl implements EventFactory {
     
     public Event onShootEvent(final Entity shooter, final Directions direction) {
         EntityFactory entityFactory = new EntityFactoryImpl();
