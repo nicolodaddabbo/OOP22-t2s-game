b694e10f324cd33907941d8c0cb957d591ee9b56
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-27 15:00:56 +0100

    [FEAT] Implemented ComponentFactory interface

---
+++ src/main/java/it/unibo/t2sgame/core/components/impl/ComponentFactoryImpl.java
@@ -0,0 +1,46 @@
+package it.unibo.t2sgame.core.components.impl;
+
+import it.unibo.t2sgame.common.Shape;
+import it.unibo.t2sgame.core.components.api.Component;
+import it.unibo.t2sgame.core.components.api.ComponentFactory;
+import it.unibo.t2sgame.input.api.Directions;
+import it.unibo.t2sgame.input.api.InputController;
+
+public class ComponentFactoryImpl implements ComponentFactory {
+
+    @Override
+    public Component createInputComponentFrom(InputController inputController) {
+        return null;
+    }
+
+    @Override
+    public Component createPhysicsComponentFrom(double speed) {
+        return new PhysicsComponent(speed);
+    }
+
+    @Override
+    public Component createPhysicsComponentFrom(double speed, Directions directions) {
+        return new PhysicsComponent(speed, directions);
+    }
+
+    @Override
+    public Component createCollisionComponentFrom(Shape shape, boolean isRigid) {
+        return new CollisionComponent(shape, isRigid);
+    }
+
+    @Override
+    public Component createHealthComponentFrom(int health) {
+        return new HealthComponent(health);
+    }
+
+    @Override
+    public Component createShootComponentFrom(double fireRate) {
+        return new ShootComponent(fireRate);
+    }
+
+    @Override
+    public Component createDamageComponentFrom(int damage, double cooldown) {
+        return new DamageComponent(damage, cooldown);
+    }
+    
+}
--- src/main/java/it/unibo/t2sgame/game/model/impl/EntityFactoryImpl.java
+++ src/main/java/it/unibo/t2sgame/game/model/impl/EntityFactoryImpl.java
@@ -1,10 +1,13 @@
 package it.unibo.t2sgame.game.model.impl;
 
 import it.unibo.t2sgame.common.Vector2D;
+import it.unibo.t2sgame.common.shapes.Circle;
 import it.unibo.t2sgame.common.shapes.Rectangle;
+import it.unibo.t2sgame.core.components.api.ComponentFactory;
 import it.unibo.t2sgame.core.components.api.GraphicComponentFactory;
 import it.unibo.t2sgame.core.components.api.InputComponentFactory;
 import it.unibo.t2sgame.core.components.impl.CollisionComponent;
+import it.unibo.t2sgame.core.components.impl.ComponentFactoryImpl;
 import it.unibo.t2sgame.core.components.impl.DamageComponent;
 import it.unibo.t2sgame.core.components.impl.GraphicComponentFactoryImpl;
 import it.unibo.t2sgame.core.components.impl.HealthComponent;
@@ -21,33 +24,35 @@ public class EntityFactoryImpl implements EntityFactory {
     private final InputComponentFactory inputFactory = new InputComponentFactoryImpl();
     private final GraphicComponentFactory graphicFactory = new GraphicComponentFactoryImpl();
 
+    private final ComponentFactory componentFactory = new ComponentFactoryImpl();
+
     @Override
     public Entity createPlayer(final Vector2D position) {
         return new EntityImpl(position)
             .addComponent(this.inputFactory.createKeyboardInputComponent())
-            .addComponent(new PhysicsComponent(1))
-            .addComponent(new CollisionComponent(new Rectangle(position, 60, 80), false))
-            .addComponent(this.graphicFactory.getPlayerGraphicComponent())
-            .addComponent(new HealthComponent(3))
-            .addComponent(new ShootComponent(1));
+            .addComponent(this.componentFactory.createPhysicsComponentFrom(1))
+            .addComponent(this.componentFactory.createCollisionComponentFrom(new Rectangle(position, 60, 80), false))
+            .addComponent(this.componentFactory.createHealthComponentFrom(3))
+            .addComponent(this.componentFactory.createShootComponentFrom(1))
+            .addComponent(this.graphicFactory.getPlayerGraphicComponent());
     }
 
     @Override
     public Entity createProjectile(final Vector2D position, final Directions direction) {
         return new EntityImpl(position)
-            .addComponent(new PhysicsComponent(1.5, direction))
-            .addComponent(new CollisionComponent(new Rectangle(position, 60, 80), false))
-            .addComponent(this.graphicFactory.getProjectileGraphicComponent())
-            .addComponent(new DamageComponent(1, 1));
+            .addComponent(this.componentFactory.createPhysicsComponentFrom(1.5, direction))
+            .addComponent(this.componentFactory.createCollisionComponentFrom(new Circle(position, 30), false))
+            .addComponent(this.componentFactory.createDamageComponentFrom(1, 1))
+            .addComponent(this.graphicFactory.getProjectileGraphicComponent());
     }
 
     @Override
     public Entity createBaseEnemy(final Vector2D position) {
         return new EntityImpl(position)
             .addComponent(this.inputFactory.createBasicEnemyAIInputComponent())
-            .addComponent(new PhysicsComponent(0.25))
-            .addComponent(new CollisionComponent(new Rectangle(position, 60, 80), false))
-            .addComponent(new DamageComponent(1, 1))
+            .addComponent(this.componentFactory.createPhysicsComponentFrom(0.25))
+            .addComponent(this.componentFactory.createCollisionComponentFrom(new Rectangle(position, 60, 80), false))
+            .addComponent(this.componentFactory.createDamageComponentFrom(1, 1))
             .addComponent(this.graphicFactory.getBaseEnemyGraphicComponent());
     }
     
