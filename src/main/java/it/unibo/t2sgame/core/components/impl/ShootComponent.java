386edeff1c2a2e4eedff57c0091ccc1d1ea5d2c8
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-27 15:28:57 +0100

    [FEAT] Now ShootComponent notifys the shoot event to the world

--- src/main/java/it/unibo/t2sgame/core/components/impl/ShootComponent.java
+++ src/main/java/it/unibo/t2sgame/core/components/impl/ShootComponent.java
@@ -33,7 +33,7 @@ public class ShootComponent extends AbstractComponent {
 
     private void shoot(final Directions shotDirection){
         if(this.timer.getElapsedSeconds() >= this.fireRate){
-            this.eventFactory.onShootEvent(this.entity, shotDirection);
+            this.entity.getWorld().ifPresent(w -> w.notifyEvent(this.eventFactory.onShootEvent(this.entity, shotDirection)));
             this.timer.restart();
         }
     }
