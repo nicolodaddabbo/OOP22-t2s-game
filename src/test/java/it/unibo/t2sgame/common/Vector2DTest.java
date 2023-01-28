6e293d741cb3eafe73144a92bff9e9a3878fdf94
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-01-28 11:04:12 +0100

    [TEST] Refactored Vector2D test

--- src/test/java/it/unibo/t2sgame/common/Vector2DTest.java
+++ src/test/java/it/unibo/t2sgame/common/Vector2DTest.java
@@ -6,29 +6,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Vector2DTest {
     
     @Test 
+    void testSum() {
+        Vector2D vector = new Vector2D(1,1);
+        double xToSum = 1.3;
+        double yToSum = -0.3;
+        assertEquals(new Vector2D(vector.getX()+xToSum, vector.getY()+yToSum), vector.sum(xToSum, yToSum));
+    }
+
+    @Test 
     void testVectorSum() {
-        //Test with x and y coordinates
-        var vector = new Vector2D(1,1);
-        assertEquals(new Vector2D(vector.getX()+1.3, vector.getY()-0.3), vector.sum(1.3, -0.3));
-        //Test with vectors
-        assertEquals(new Vector2D(vector.getX()+1.3, vector.getY()-0.3), vector.sum(new Vector2D(1.3, -0.3)));
+        Vector2D vector = new Vector2D(1,1);
+        Vector2D vectorToSum = new Vector2D(1.3,-0.3);
+        assertEquals(new Vector2D(vector.getX()+vectorToSum.getX(), vector.getY()+vectorToSum.getY()), vector.sum(vectorToSum));
+    }
+
+    @Test 
+    void testSub() {
+        Vector2D vector = new Vector2D(1,1);
+        double xToSub = 1.3;
+        double yToSub = -0.3;
+        assertEquals(new Vector2D(vector.getX()-xToSub, vector.getY()-yToSub), vector.sub(xToSub, yToSub));
     }
 
     @Test 
     void testVectorSub() {
-        //Test with x and y coordinates
-        var vector = new Vector2D(1,1);
-        assertEquals(new Vector2D(vector.getX()-1.3, vector.getY()+0.3), vector.sub(1.3, -0.3));
-        //Test with vectors
-        assertEquals(new Vector2D(vector.getX()-1.3, vector.getY()+0.3), vector.sub(new Vector2D(1.3, -0.3)));
+        Vector2D vector = new Vector2D(1,1);
+        Vector2D vectorToSub = new Vector2D(1.3,-0.3);
+        assertEquals(new Vector2D(vector.getX()-vectorToSub.getX(), vector.getY()-vectorToSub.getY()), vector.sub(vectorToSub));
     }
 
     @Test 
-    void testVectorMul() {
-        var vector = new Vector2D(1,1);
-        assertEquals(new Vector2D(vector.getX()*2, vector.getY()*2), vector.mul(2));
-        assertEquals(new Vector2D(vector.getX()*-1, vector.getY()*-1), vector.mul(-1));
-        assertEquals(new Vector2D(vector.getX()*0, vector.getY()*0), vector.mul(0));
+    void testMul() {
+        Vector2D vector = new Vector2D(1,1);
+        double scalar = 0;
+        assertEquals(new Vector2D(vector.getX()*scalar, vector.getY()*scalar), vector.mul(scalar));
+    }
+
+    @Test 
+    void testDistance() {
+        Vector2D vector = new Vector2D(1,1);
+        double x = 2;
+        double y = 3;
+        assertEquals(Math.sqrt(Math.pow(vector.getX()-x, 2)+Math.pow(vector.getY()-y, 2)), vector.distance(x, y));
+    }
+
+    @Test 
+    void testVectorDistance() {
+        Vector2D firstVector = new Vector2D(1,1);
+        Vector2D secondVector = new Vector2D(2, 3);
+        assertEquals(Math.sqrt(Math.pow(firstVector.getX()-secondVector.getX(), 2)+Math.pow(firstVector.getY()-secondVector.getY(), 2)), firstVector.distance(secondVector));
     }
 
 }
