47d353273fd608b3742f1bbba8aaf6be0d4383c4
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-01-30 12:20:15 +0100

    [REFACTOR] changed GraphicComponent into an abstract class

--- src/main/java/it/unibo/t2sgame/core/components/impl/GraphicComponent.java
+++ src/main/java/it/unibo/t2sgame/core/components/impl/GraphicComponent.java
@@ -2,16 +2,13 @@ package it.unibo.t2sgame.core.components.impl;
 
 import it.unibo.t2sgame.core.components.api.AbstractComponent;
 import it.unibo.t2sgame.core.components.api.Message;
-import it.unibo.t2sgame.core.entity.api.Entity;
 import it.unibo.t2sgame.view.api.Graphic;
 
-public class GraphicComponent extends AbstractComponent{
+public abstract class GraphicComponent extends AbstractComponent{
     protected Graphic graphic;
-    protected Entity entity;
 
     @Override
     public void update() {
-        // TODO Auto-generated method stub
         
     }
 
@@ -21,13 +18,5 @@ public class GraphicComponent extends AbstractComponent{
     @Override
     public <T> void receive(Message<T> messge) {
         
-    }
-
-    public Entity getEntity() {
-        return this.entity;
-    }
-
-    public void setEntity(Entity entity) {
-        this.entity = entity;     
     }
 }
