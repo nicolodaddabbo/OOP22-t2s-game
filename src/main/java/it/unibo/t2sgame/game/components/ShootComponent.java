5bcffd719ac3568bd04d5e3ff61bba1130b7c90a
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-01-31 11:54:54 +0100

    [REFACTOR] Change the reference from a generic Enviroment to the World interface

--- src/main/java/it/unibo/t2sgame/game/components/ShootComponent.java
+++ src/main/java/it/unibo/t2sgame/game/components/ShootComponent.java
@@ -33,7 +33,7 @@ public class ShootComponent extends AbstractComponent {
 
     private void shoot(final Directions shotDirection){
         if(this.timer.getElapsedSeconds() >= this.fireRate){
-            this.entity.getEnviroment().ifPresent(e -> e.notifyEvent(this.eventFactory.onShootEvent(this.entity, shotDirection)));
+            this.entity.getWorld().ifPresent(e -> e.notifyEvent(this.eventFactory.onShootEvent(this.entity, shotDirection)));
             this.timer.restart();
         }
     }
