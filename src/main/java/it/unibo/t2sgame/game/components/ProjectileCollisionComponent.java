e7d807c3d7bcc782ee1ee51e1df2d5946c5d6be2
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-01 13:00:37 +0100

    [REFACOTR] Made ProjectileCollision extend CollisionComponent instead of BaseCollision since a projectile doesnt have health

--- src/main/java/it/unibo/t2sgame/game/components/ProjectileCollisionComponent.java
+++ src/main/java/it/unibo/t2sgame/game/components/ProjectileCollisionComponent.java
@@ -3,14 +3,15 @@ package it.unibo.t2sgame.game.components;
 import java.util.List;
 
 import it.unibo.t2sgame.common.Shape;
+import it.unibo.t2sgame.core.components.impl.CollisionComponent;
 import it.unibo.t2sgame.core.entity.api.Entity;
 import it.unibo.t2sgame.game.components.TypeComponent.Type;
 import it.unibo.t2sgame.game.logics.api.EventFactory;
 import it.unibo.t2sgame.game.logics.impl.EventFactoryImpl;
 
-public class ProjectileCollisionComponent extends BaseCollisionComponent {
+public class ProjectileCollisionComponent extends CollisionComponent {
 
-    private EventFactory eventFactory = new EventFactoryImpl();
+    private final EventFactory eventFactory = new EventFactoryImpl();
 
     public ProjectileCollisionComponent(Shape shape, boolean isRigid, List<Type> types) {
         super(shape, isRigid, types);
@@ -18,7 +19,6 @@ public class ProjectileCollisionComponent extends BaseCollisionComponent {
 
     @Override
     protected void collisionAction(Entity entity) {
-        super.collisionAction(entity);
         this.entity.getWorld().ifPresent(e -> e.notifyEvent(this.eventFactory.onDeathEvent(this.entity)));
     }
     
