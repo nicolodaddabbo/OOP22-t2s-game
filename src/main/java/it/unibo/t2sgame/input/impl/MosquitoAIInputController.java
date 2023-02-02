c9573f9e5b60b3d6bc0d4984b59477354fa7a174
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-02 17:01:10 +0100

    [FEAT] Added Mosquito AI input controller

---
+++ src/main/java/it/unibo/t2sgame/input/impl/MosquitoAIInputController.java
@@ -0,0 +1,32 @@
+package it.unibo.t2sgame.input.impl;
+
+import java.util.Random;
+
+import it.unibo.t2sgame.core.components.impl.PhysicsComponent;
+import it.unibo.t2sgame.input.api.AbstractAIInputController;
+import it.unibo.t2sgame.input.api.Directions;
+
+/**
+ * AI that randomly change direction every TIME_TO_NEXT_DECISION ms 
+ */
+public class MosquitoAIInputController extends AbstractAIInputController {
+    private final Random random = new Random();
+    private static final long TIME_TO_NEXT_DECISION = 500;
+    private long lastChoiceTime = 0;
+
+    @Override
+    protected void computeNextCommand() {
+        this.commandsQueue.add(entity -> {
+            if (isTimeToChoose()) {
+                entity.notifyComponent(PhysicsComponent.class,
+                    () -> Directions.values()[random.nextInt(Directions.values().length)]);
+                this.lastChoiceTime = System.currentTimeMillis();
+            }
+        });
+    }
+
+    private boolean isTimeToChoose(){
+		return System.currentTimeMillis() - lastChoiceTime > TIME_TO_NEXT_DECISION;
+	}
+
+}
