f42278cc07ac75da687389120855c35f5ff52401
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-02 12:22:14 +0100

    [FEAT] Added a method to get the players' input controllers and set them to the view

--- src/main/java/it/unibo/t2sgame/core/engine/impl/GameEngineImpl.java
+++ src/main/java/it/unibo/t2sgame/core/engine/impl/GameEngineImpl.java
@@ -1,11 +1,7 @@
 package it.unibo.t2sgame.core.engine.impl;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
 import it.unibo.t2sgame.common.StopWatch;
 import it.unibo.t2sgame.core.components.api.Component;
@@ -14,34 +10,37 @@ import it.unibo.t2sgame.core.components.impl.InputComponent;
 import it.unibo.t2sgame.core.components.impl.PhysicsComponent;
 import it.unibo.t2sgame.core.engine.api.GameEngine;
 import it.unibo.t2sgame.game.Game;
+import it.unibo.t2sgame.input.impl.KeyboardInputController;
 import it.unibo.t2sgame.view.api.GameScene;
 
 public class GameEngineImpl implements GameEngine {
-    /**
+    /*
      * This long indicates the perod of updating the states systems in nanoseconds
      */
     private static final long NS_FRAME_PERIOD = (long) (7 * 1E6);
-
-    /**
+    /*
      * This long indicates the perod of updating the states systems in nanoseconds
      */
     private static final long NS_UPDATE_PERDIOD = (long) (7 * 1E6);
-    /**
+    /*
      * The game which is hosted by the engine
      */
     private final Game game;
-    /**
+    /*
      * The view where the engine will render the game
      */
     private final GameScene view;
-
     /* Usefull to synchronize states systems and to wait for next frame */
     private final StopWatch timer = new StopWatch().start();
     /* Represent the "gap" between game time and real time */
     private long lag = 0;
 
-    private final Map<Class<Component>,List<Component>> systems = new HashMap<>();
-
+    /**
+     * Create a GameEngine's istance based on {@link scene} and {@link game}
+     * 
+     * @param scene the scene which is delegated to render by the game engine
+     * @param game  the game hosted by the engine
+     */
     public GameEngineImpl(GameScene scene, Game game) {
         this.view = scene;
         this.game = game;
@@ -49,6 +48,7 @@ public class GameEngineImpl implements GameEngine {
 
     @Override
     public void run() {
+        this.init();
         while (!this.game.isOver()) {
             this.lag = this.lag + this.timer.getElapsedNanos();
             this.timer.restart();
@@ -75,12 +75,6 @@ public class GameEngineImpl implements GameEngine {
         return this.game;
     }
 
-    /**
-     * Update the component with {@link clazz} type
-     * 
-     * @param <T>   the type of the component to get
-     * @param clazz the class of the component to get
-     */
     @Override
     public <T extends Component> void updateComponentBy(Class<T> clazz) {
         this.getComponents(clazz).forEach(Component::update);
@@ -91,12 +85,7 @@ public class GameEngineImpl implements GameEngine {
         this.getComponents(clazz).stream().parallel().forEach(Component::update);
     }
 
-    /**
-     * 
-     * @param <T>
-     * @param clazz
-     * @return
-     */
+    @Override
     public <T extends Component> List<T> getComponents(Class<T> clazz) {
         return this.game.getWorld().getEntities().stream()
                 .map(e -> e.getComponent(clazz))
@@ -105,20 +94,43 @@ public class GameEngineImpl implements GameEngine {
                 .toList();
     }
 
-
     private void updateGame() {
         this.updateComponentByConcurrent(PhysicsComponent.class);
         this.updateComponentByConcurrent(CollisionComponent.class);
+        // Update the game logics and check events
         this.game.update();
     }
 
-    /**
-     * 
-     * @return true if the game engine is synchronized with the real time,
-     *         otherwise false
+    /*
+     * Getting the input controllers of the players
+     */
+    private List<KeyboardInputController> getKeyboardInputController() {
+        return this.game.getWorld().getPlayers().stream()
+                // Mapping every player with its input component
+                .map(p -> p.getComponent(InputComponent.class))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                // Map each InputComponent with its InputController
+                .map(InputComponent::getInputController)
+                // Cast to KeyboardInputController
+                .filter(KeyboardInputController.class::isInstance)
+                .map(KeyboardInputController.class::cast)
+                // Collect the input controllers in a list
+                .toList();
+    }
+
+    /*
+     * Check if the "game time" is synchronized with the "real time"
      */
     private boolean isSync() {
         return this.lag < NS_UPDATE_PERDIOD;
+    }
+
+    /*
+     * Initialize the engine before start to running
+     */
+    private void init() {
+        this.view.setInputControllers(this.getKeyboardInputController());
     }
 
     /**
