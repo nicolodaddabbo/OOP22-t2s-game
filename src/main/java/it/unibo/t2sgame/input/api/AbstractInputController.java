0c4a94ed624b892a06cfd51c0a536c0766afd43b
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-02 16:19:20 +0100

    [DOCS] Added comments to AbstractInputController

--- src/main/java/it/unibo/t2sgame/input/api/AbstractInputController.java
+++ src/main/java/it/unibo/t2sgame/input/api/AbstractInputController.java
@@ -3,9 +3,16 @@ package it.unibo.t2sgame.input.api;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+/**
+ * This abstract class represent an InutController with a default implementation of
+ * the method getCommandsQueue
+ */
 public abstract class AbstractInputController implements InputController {
     protected final Queue<Command> commandsQueue = new ConcurrentLinkedQueue<>();
 
+    /**
+     * Return a defensive copy of the commands queue
+     */
     @Override
     public Queue<Command> getCommandsQueue() {
         var defensiveQueue = new ConcurrentLinkedQueue<>(this.commandsQueue);
