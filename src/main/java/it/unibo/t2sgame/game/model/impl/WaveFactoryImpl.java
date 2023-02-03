3abb3997360d7922374a1faabec6343914cb02b1
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-02-03 13:31:44 +0100

    [REFACTOR] now the enemies spawn inside the map

--- src/main/java/it/unibo/t2sgame/game/model/impl/WaveFactoryImpl.java
+++ src/main/java/it/unibo/t2sgame/game/model/impl/WaveFactoryImpl.java
@@ -1,28 +1,28 @@
 package it.unibo.t2sgame.game.model.impl;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import it.unibo.t2sgame.common.Vector2D;
-import it.unibo.t2sgame.core.components.impl.CollisionComponent;
-import it.unibo.t2sgame.core.components.impl.PhysicsComponent;
 import it.unibo.t2sgame.core.entity.api.Entity;
-import it.unibo.t2sgame.game.Game;
-import it.unibo.t2sgame.game.components.DamageComponent;
-import it.unibo.t2sgame.game.components.HealthComponent;
-import it.unibo.t2sgame.game.logics.api.State;
 import it.unibo.t2sgame.game.model.api.EntityFactory;
 import it.unibo.t2sgame.game.model.api.Wave;
 import it.unibo.t2sgame.game.model.api.WaveFactory;
+import it.unibo.t2sgame.game.model.api.World;
 
 public class WaveFactoryImpl implements WaveFactory {
     private EntityFactory entityFactory = new EntityFactoryImpl();
-    private Random random = new Random(); 
+    private Random random = new Random();
+    private World world; 
+    private double width, height;
+
+    public WaveFactoryImpl(final World world){
+        this.world = world;
+        this.width = this.world.getMap().getWidth();
+        this.height = this.world.getMap().getHeight();
+    }
 
     private Wave createWaveFromEnemies(final List<Entity> enemies) {
         return new Wave() {
@@ -42,22 +42,9 @@ public class WaveFactoryImpl implements WaveFactory {
     @Override
     public Wave createBasicWave(final int round) {
         List<Entity> enemies;
-        enemies = Stream.generate(() -> entityFactory.createBaseEnemy(new Vector2D(random.nextDouble(1000), random.nextDouble(1000))))
+        enemies = Stream.generate(() -> entityFactory.createBaseEnemy(new Vector2D(random.nextDouble(0, this.width), random.nextDouble(0, this.height))))
             .limit(round)
             .collect(Collectors.toList());
-        
-
         return createWaveFromEnemies(enemies);
     }
-
-    public Entity createBoss(){
-        var boss = entityFactory.createBaseEnemy(new Vector2D(random.nextDouble(1000), random.nextDouble(1000)));
-
-        boss.getComponent(PhysicsComponent.class).ifPresent(c -> c.setSpeed(c.getSpeed()*2));
-        boss.getComponent(DamageComponent.class).ifPresent(c -> c.setDamage(c.getDamage()+1));
-        boss.getComponent(HealthComponent.class).ifPresent(c -> c.setHealth(c.getHealth()*2));
-        //boss.getComponent(GraphicComponent.class).ifPresent(c -> c.);
-        return boss;
-    }
-
 }
