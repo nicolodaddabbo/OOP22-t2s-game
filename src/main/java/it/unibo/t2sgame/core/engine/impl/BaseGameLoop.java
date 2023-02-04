dbfe27f84e0a7354facc990677cf6f0ec2d2ba00
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-04 14:20:31 +0100

    [REFACTOR] Created an abstract class representing the base game loop as input of decorator

---
+++ src/main/java/it/unibo/t2sgame/core/engine/impl/BaseGameLoop.java
@@ -0,0 +1,50 @@
+package it.unibo.t2sgame.core.engine.impl;
+
+import java.util.function.Consumer;
+
+import it.unibo.t2sgame.core.components.api.Component;
+import it.unibo.t2sgame.core.components.impl.CollisionComponent;
+import it.unibo.t2sgame.core.components.impl.InputComponent;
+import it.unibo.t2sgame.core.components.impl.PhysicsComponent;
+import it.unibo.t2sgame.core.engine.api.GameEngine;
+import it.unibo.t2sgame.core.engine.api.GameLoop;
+
+/**
+ * This abstract class models the base of every game loop.
+ * Based on its updater Consumer, the game loop update the components passed as
+ * input of the Consumer.
+ * The type of updater Consumer depends on the real implementation of the GameLoop
+ */
+abstract class AbstractGameLoop implements GameLoop {
+
+    protected final GameEngineImpl engine;
+
+    public AbstractGameLoop(GameEngineImpl engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public GameEngine getEngine() {
+        return this.engine;
+    }
+
+    @Override
+    public void processInput() {
+        this.getUpdater().accept(InputComponent.class);
+    }
+
+    @Override
+    public void render() {
+        this.engine.getScene().render();
+    }
+
+    @Override
+    public void updateGame() {
+        this.getUpdater().accept(PhysicsComponent.class);
+        this.getUpdater().accept(CollisionComponent.class);
+        this.engine.getGame().update();
+    }
+
+    abstract Consumer<Class<? extends Component>> getUpdater();
+
+}
---
+++ src/main/java/it/unibo/t2sgame/core/engine/impl/ConcurrentGameLoop.java
@@ -0,0 +1,27 @@
+package it.unibo.t2sgame.core.engine.impl;
+
+import java.util.function.Consumer;
+
+import it.unibo.t2sgame.core.components.api.Component;
+
+/**
+ * This class extends the base game loop implementation.
+ * This class implements getUpdater abstract method with a non
+ * concurrent-updater, in order
+ * to be used by the template methods in BaseGameLoop.
+ * It's not preferable to use this class in order to achieve better
+ * performances.
+ */
+class ConcurrentGameLoop extends BaseGameLoop{
+
+    public ConcurrentGameLoop(GameEngineImpl engine) {
+        super(engine);
+    }
+
+    @Override
+    Consumer<Class<? extends Component>> getUpdater() {
+        return this.engine::updateComponentByConcurrent;
+    }
+
+   
+}
