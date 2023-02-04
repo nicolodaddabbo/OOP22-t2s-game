ba887d465a594915ab45188aac9a3ed3793729a0
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-02-04 12:31:21 +0100

    [DOCS] added documentation to interface Window

--- src/main/java/it/unibo/t2sgame/view/api/Window.java
+++ src/main/java/it/unibo/t2sgame/view/api/Window.java
@@ -1,14 +1,26 @@
 package it.unibo.t2sgame.view.api;
 
+/**
+ * Interface that represents the concept of a Window (a container of scenes)
+ */
 public interface Window {
+    /**
+     * method called to launch the Window
+     */
     void launch();
     /**
-     * 
-     * @return a created Scene
+     * method called to create a GameScene in the Window that called this method
+     * @return the GameScene
      */
     GameScene createGameScene();
-
+    /**
+     * method called to create a BaseScene that represents a Menu in the Window that called this method
+     * @return the MenuScene
+     */
     BaseScene createMenuScene();
-
+    /**
+     * method called to create a BaseScene that represents a GameOverScene in the Window that called this method
+     * @return the GameScene
+     */
     BaseScene createGameOverScene(int round);
 }
