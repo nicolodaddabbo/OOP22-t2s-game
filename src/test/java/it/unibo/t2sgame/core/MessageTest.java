0561347acf2939bdac61951308bf9b908db36ac3
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-04 10:59:34 +0100

    [TEST] Changed MessageTest

--- src/test/java/it/unibo/t2sgame/core/MessageTest.java
+++ src/test/java/it/unibo/t2sgame/core/MessageTest.java
@@ -3,6 +3,9 @@ package it.unibo.t2sgame.core;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.List;
 
 import it.unibo.t2sgame.common.Vector2D;
 import it.unibo.t2sgame.common.shapes.Rectangle;
@@ -11,70 +14,73 @@ import it.unibo.t2sgame.core.components.impl.CollisionComponent;
 import it.unibo.t2sgame.core.components.impl.ComponentFactoryImpl;
 import it.unibo.t2sgame.core.components.impl.PhysicsComponent;
 import it.unibo.t2sgame.core.entity.api.Entity;
+import it.unibo.t2sgame.core.entity.api.Type;
 import it.unibo.t2sgame.core.entity.impl.EntityImpl;
-import it.unibo.t2sgame.game.components.HealthComponent;
 import it.unibo.t2sgame.input.api.Directions;
 
 public class MessageTest {
 
     private ComponentFactory componentFactory = new ComponentFactoryImpl();
 
-    private Entity entityWithPhysics(){
-        Entity entity = new EntityImpl(new Vector2D(0,0)).addComponent(this.componentFactory.createPhysicsComponentFrom(1));
-        return entity;
-    }
-
-    private Entity entityWithCollision(){
+    private Entity baseEntity(){
         var pos = new Vector2D(0,0);
-        Entity entity = new EntityImpl(pos).addComponent(this.componentFactory.createCollisionComponentFrom(new Rectangle(pos, 0, 0), false));
-        return entity;
-    }
-
-    private Entity entityWithPhysicsAndCollision(){
-        var pos = new Vector2D(0,0);
-        Entity entity = new EntityImpl(pos).addComponent(this.componentFactory.createPhysicsComponentFrom(1))
-            .addComponent(this.componentFactory.createCollisionComponentFrom(new Rectangle(pos, 0, 0), false));
-        return entity;
-    }
-
-    private Entity entityWithHealth(){
-        Entity entity = new EntityImpl(new Vector2D(0,0)).addComponent(this.componentFactory.createHealthComponentFrom(3));
-        return entity;
+        return new EntityImpl(pos, Type.PLAYER)
+            .addComponent(this.componentFactory.createPhysicsComponentFrom(1))
+            .addComponent(this.componentFactory.createCollisionComponentFrom(new Rectangle(pos, 0, 0), false, List.of()));
     }
 
     @Test
     void testPhysicsReceiveDirection(){
-        var entity = entityWithPhysics();
+        var entity = this.baseEntity();
+        //Notifys the direction
         entity.notifyComponent(PhysicsComponent.class, () -> Directions.UP);
         var physComponent = entity.getComponent(PhysicsComponent.class).get();
-        physComponent.update();
+        //The velocity should be the same as the given direction
         assertEquals(new Vector2D(0, -1), physComponent.getVelocity());
     }
 
     @Test
+    void testPhysicsReceiveUnwantedMessage(){
+        var entity = this.baseEntity();
+        //Notifys unnecepted message
+        entity.notifyComponent(PhysicsComponent.class, () -> "Go up");
+        var physComponent = entity.getComponent(PhysicsComponent.class).get();
+        //The velocity should be the unchanged
+        assertNotEquals(new Vector2D(0, -1), physComponent.getVelocity());
+    }
+
+    @Test
     void testCollisionReceivePosition(){
-        var entity = entityWithCollision();
+        var entity = this.baseEntity();
         var pos = new Vector2D(5, 5);
+        //Notifys the new position
         entity.notifyComponent(CollisionComponent.class, () -> pos);
         var collComponent = entity.getComponent(CollisionComponent.class).get();
+        //The position of the collisions center should be the same as the nitified position
         assertEquals(pos, collComponent.getShape().getCenter());
     }
 
     @Test
-    void testHealthReceiveDamage(){
-        var entity = entityWithHealth();
-        entity.notifyComponent(HealthComponent.class, () -> 2);
-        var hpComponent = entity.getComponent(HealthComponent.class).get();
-        assertEquals(1, hpComponent.getHealth());
+    void testCollisionReceiveUnwantedMessage(){
+        var entity = this.baseEntity();
+        var pos = new Vector2D(5, 5);
+        //Notifys unnecepted message
+        entity.notifyComponent(CollisionComponent.class, () -> "Go to position 5:5");
+        var collComponent = entity.getComponent(CollisionComponent.class).get();
+        //The position of the collisions center should be unchanged
+        assertNotEquals(pos, collComponent.getShape().getCenter());
     }
 
     @Test
     void testComunicationPhysicsWithCollision(){
-        var entity = entityWithPhysicsAndCollision();
+        var entity = this.baseEntity();
+        //Notifys the direction
         entity.notifyComponent(PhysicsComponent.class, () -> Directions.UP);
         var physComponent = entity.getComponent(PhysicsComponent.class).get();
+        //Notifys the collision component of the new position
         physComponent.update();
         var collComponent = entity.getComponent(CollisionComponent.class).get();
+        //The position of the entity should be the same as the collisions center
         assertEquals(entity.getPosition(), collComponent.getShape().getCenter());
     }
 }
