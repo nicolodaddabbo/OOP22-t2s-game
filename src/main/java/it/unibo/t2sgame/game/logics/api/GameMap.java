1f6c8e09ce60a7c627bb19fd1bb0db43f8180f66
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-02-04 12:03:25 +0100

    [DOCS] added documentation for the interface GameMap

--- src/main/java/it/unibo/t2sgame/game/logics/api/GameMap.java
+++ src/main/java/it/unibo/t2sgame/game/logics/api/GameMap.java
@@ -1,9 +1,16 @@
 package it.unibo.t2sgame.game.logics.api;
 
+/**
+ * This interface models the concept of GameMap
+ */
 public interface GameMap {
-    
+    /**
+     * @return the width of the GameMap
+     */
     double getWidth();
-
+    /**
+     * @return the height of the GameMap
+     */
     double getHeight();
 
 }
