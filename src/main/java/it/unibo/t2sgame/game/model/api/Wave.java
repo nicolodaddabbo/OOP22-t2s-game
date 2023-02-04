b9e58a66b0a9fb56b4e14bbfcdb02bb6d98a703f
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-02-04 12:04:03 +0100

    [DOCS] adde documentation for the interface Wave

--- src/main/java/it/unibo/t2sgame/game/model/api/Wave.java
+++ src/main/java/it/unibo/t2sgame/game/model/api/Wave.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import it.unibo.t2sgame.core.entity.api.Entity;
 
+/**
+ * interface that represents the concept of a Wave of enemies
+ */
 public interface Wave {
-    
     /**
      * 
      * @return a list of Entity representing a Wave
