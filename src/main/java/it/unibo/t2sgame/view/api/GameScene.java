2c73993aa59f76af35f470e313cab7effba81eb4
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-02-04 12:25:14 +0100

    [DOCS] added documentation to interface GameScene

--- src/main/java/it/unibo/t2sgame/view/api/GameScene.java
+++ src/main/java/it/unibo/t2sgame/view/api/GameScene.java
@@ -18,12 +18,20 @@ public interface GameScene extends BaseScene{
      * @param gameEngine
      */
     void setEngine(GameEngine gameEngine);
-
+    /**
+     * @return the game in which the GameScene was created 
+     */
     Game getGame();
-
+    /**
+     * @return the Graphic architecture used
+     */
     Graphic getGraphic();
-
+    /**
+     * setter to receive the list of KeyboardInputControllers of the players
+     */
     void setInputControllers(List<KeyboardInputController> keyInControllers);
-
+    /**
+     * method called when the game is over, this method calls the rendering of a GameOverScene
+     */
     void gameOver();
 }
