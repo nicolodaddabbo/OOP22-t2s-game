50d4bd7ed46870c50a5691565d1d7bb57311c402
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-02-04 11:59:14 +0100

    [DOCS] added documentation for GraphicComponent

--- src/main/java/it/unibo/t2sgame/core/components/impl/GraphicComponent.java
+++ src/main/java/it/unibo/t2sgame/core/components/impl/GraphicComponent.java
@@ -3,11 +3,17 @@ package it.unibo.t2sgame.core.components.impl;
 import it.unibo.t2sgame.core.components.api.AbstractComponent;
 import it.unibo.t2sgame.core.components.api.Message;
 import it.unibo.t2sgame.view.api.Graphic;
-
+/**
+ * this abstract class represents the GraphicComponent of the entity
+ */
 public abstract class GraphicComponent extends AbstractComponent{
     protected Graphic graphic;
-    protected final double width, height;
-    
+    protected final double width;
+    protected final double height;
+    /**
+     * @param width the width of the GraphicComponent
+     * @param height the height of the GraphicComponent
+     */
     protected GraphicComponent(final double width, final double height) {
         this.width = width;
         this.height = height;
@@ -17,19 +23,27 @@ public abstract class GraphicComponent extends AbstractComponent{
     public void update() {
         
     }
-
+    /**
+     * setter for the Graphic architecture used
+     * @param graphic the graphic architecture used
+     */
     public void setGraphics(Graphic graphic) {
         this.graphic = graphic;   
     }
+
     @Override
     public <T> void receive(Message<T> messge) {
         
     }
-
+    /**
+     * @return the width of the GraphicComponent
+     */
     public double getWidth() {
         return width;
     }
-
+    /**
+     * @return the height of the GraphicComponent
+     */
     public double getHeight() {
         return height;
     }
