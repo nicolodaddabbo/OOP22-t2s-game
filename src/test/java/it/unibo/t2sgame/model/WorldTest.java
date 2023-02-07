14358400c25acb1e08dd5964440cbf9bb00664f2
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-07 16:09:24 +0100

    [TEST] Testing basicWorld, singlePlayerWorld and WorldWithMorePlayers

--- src/test/java/it/unibo/t2sgame/model/WorldTest.java
+++ src/test/java/it/unibo/t2sgame/model/WorldTest.java
@@ -6,10 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.lang.StackWalker.Option;
 import java.util.List;
 import java.util.Optional;
 
