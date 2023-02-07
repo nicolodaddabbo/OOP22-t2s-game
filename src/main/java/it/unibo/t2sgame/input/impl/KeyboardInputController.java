a7d1f1054ba43b40fdffb7f1b04769f8d86d2a2a
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 16:53:31 +0100

    [DOCS] Added Javadoc comments

--- src/main/java/it/unibo/t2sgame/input/impl/KeyboardInputController.java
+++ src/main/java/it/unibo/t2sgame/input/impl/KeyboardInputController.java
@@ -9,6 +9,9 @@ import it.unibo.t2sgame.input.api.Command;
 import it.unibo.t2sgame.input.api.Directions;
 import it.unibo.t2sgame.input.api.EntityState;
 
+/**
+ * Input controller with a keyboard as input source.
+ */
 public class KeyboardInputController extends AbstractInputController {
     private static final int MOVE_UP_CODE = 87;
     private static final int MOVE_RIGHT_CODE = 68;
@@ -20,28 +23,36 @@ public class KeyboardInputController extends AbstractInputController {
     private static final int SHOOT_LEFT_CODE = 37;
 
     private static final Map<Integer, Command> WALK_MOVESET = Map.of(
-        MOVE_UP_CODE, new Move(Directions.UP),
-        MOVE_RIGHT_CODE, new Move(Directions.RIGHT),
-        MOVE_DOWN_CODE, new Move(Directions.DOWN),
-        MOVE_LEFT_CODE, new Move(Directions.LEFT)
-    );
+            MOVE_UP_CODE, new Move(Directions.UP),
+            MOVE_RIGHT_CODE, new Move(Directions.RIGHT),
+            MOVE_DOWN_CODE, new Move(Directions.DOWN),
+            MOVE_LEFT_CODE, new Move(Directions.LEFT));
 
     private static final Map<Integer, Command> SHOOT_MOVESET = Map.of(
-        SHOOT_UP_CODE, new Shoot(Directions.UP),
-        SHOOT_RIGHT_CODE, new Shoot(Directions.RIGHT),
-        SHOOT_DOWN_CODE, new Shoot(Directions.DOWN),
-        SHOOT_LEFT_CODE, new Shoot(Directions.LEFT)
-    );
+            SHOOT_UP_CODE, new Shoot(Directions.UP),
+            SHOOT_RIGHT_CODE, new Shoot(Directions.RIGHT),
+            SHOOT_DOWN_CODE, new Shoot(Directions.DOWN),
+            SHOOT_LEFT_CODE, new Shoot(Directions.LEFT));
 
     private final EntityState<Integer> moveState = new EntityStateImpl<>(WALK_MOVESET);
     private final EntityState<Integer> shootState = new EntityStateImpl<>(SHOOT_MOVESET);
     private final List<EntityState<Integer>> states = List.of(this.moveState, this.shootState);
 
+    /**
+     * Notify that a key has been pressed
+     * 
+     * @param keyCode of the key pressed
+     */
     public void notifyKeyPressed(final int keyCode) {
         this.states.forEach(s -> s.notifyInput(keyCode));
         addCommandsToQueue();
     }
 
+    /**
+     * Notify that a key has been released
+     * 
+     * @param keyCode of the key released
+     */
     public void notifyKeyReleased(final int keyCode) {
         this.moveState.notifyInputRelease(keyCode, Optional.of(new Move(Directions.STAY)));
         this.shootState.notifyInputRelease(keyCode, Optional.empty());
@@ -49,7 +60,8 @@ public class KeyboardInputController extends AbstractInputController {
     }
 
     private void addCommandsToQueue() {
-        this.commandsQueue.addAll(this.states.stream().filter(s -> s.getCurrentCommand().isPresent()).map(s -> s.getCurrentCommand().get()).toList());
+        this.commandsQueue.addAll(this.states.stream().filter(s -> s.getCurrentCommand().isPresent())
+                .map(s -> s.getCurrentCommand().get()).toList());
     }
 
 }
