d3a959d9b2822a8ce7ad9f950314457c8be8e06a
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 17:49:30 +0100

    [DOCS] Added constructor Javadoc comment

--- src/main/java/it/unibo/t2sgame/input/impl/Move.java
+++ src/main/java/it/unibo/t2sgame/input/impl/Move.java
@@ -7,17 +7,20 @@ import it.unibo.t2sgame.input.api.Directions;
 
 /**
  * This class is used to handle all kind of move related commands
- * which differs only in the direction of the movement
+ * which differs only in the direction of the movement.
  */
 public class Move implements Command {
     private final Directions direction;
 
+    /**
+     * @param direction of the movement
+     */
     public Move(final Directions direction) {
         this.direction = direction;
     }
 
     /**
-     * Notify the entity's Physic component comunicating the direction of the movement
+     * Notify the entity's Physic component comunicating the direction of the movement.
      */
     @Override
     public void execute(final Entity entity) {
