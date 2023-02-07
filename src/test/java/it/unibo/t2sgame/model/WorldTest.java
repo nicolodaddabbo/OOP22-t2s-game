ce3e047a5ae2b2b678ff8210f939b7f37d4d9a66
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-07 14:15:33 +0100

    [TEST] Testing basicWorld, singlePlayerWorld and WorldWithMorePlayers

--- src/test/java/it/unibo/t2sgame/model/WorldTest.java
+++ src/test/java/it/unibo/t2sgame/model/WorldTest.java
@@ -1,64 +1,89 @@
 package it.unibo.t2sgame.model;
 
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.StackWalker.Option;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
 import it.unibo.t2sgame.common.Vector2D;
-import it.unibo.t2sgame.input.api.InputComponent;
-import it.unibo.t2sgame.model.api.Entity;
-import it.unibo.t2sgame.model.api.EntityFactory;
-import it.unibo.t2sgame.model.api.Wave;
-import it.unibo.t2sgame.model.api.World;
-import it.unibo.t2sgame.model.api.WorldFactory;
-import it.unibo.t2sgame.model.impl.EntityFactoryImpl;
-import it.unibo.t2sgame.model.impl.EntityImpl;
-import it.unibo.t2sgame.model.impl.WaveFactoryImpl;
-import it.unibo.t2sgame.model.impl.WorldFactoryImpl;
+import it.unibo.t2sgame.game.model.api.EntityFactory;
+import it.unibo.t2sgame.game.model.api.WaveFactory;
+import it.unibo.t2sgame.game.model.api.World;
+import it.unibo.t2sgame.game.model.api.WorldFactory;
+import it.unibo.t2sgame.game.model.impl.EntityFactoryImpl;
+import it.unibo.t2sgame.game.model.impl.WaveFactoryImpl;
+import it.unibo.t2sgame.game.model.impl.WorldFactoryImpl;
+
 
 public class WorldTest {
     
-    private WorldFactory worldFactory = new WorldFactoryImpl();
-    private EntityFactory entityFactory = new EntityFactoryImpl();
-    private WaveFactoryImpl waveFactory = new WaveFactoryImpl();
-
-    private void basics(final World w){
-        // Setting a wave
-        w.setWave(new WaveFactoryImpl().createBasicWave(1));
-        // Check if getPlayers return a list of entity with an Input Component
-        assertTrue(w.getPlayers().stream().map(p -> p.getComponent(InputComponent.class).isPresent()).reduce((r1,r2) -> (r1 && r2)).get());
-        // Check if getCurrentWave returns a set of entity with an InputComponent
-         assertTrue(w.getCurrentWave().getEnemies().stream().map(e -> e.getComponent(InputComponent.class).isPresent()).reduce((r1,r2) -> (r1 && r2)).get());
-        // Checking set a new Wave
-        Wave wave = this.waveFactory.createBasicWave(2);
-        w.setWave(wave);
-        assertEquals(wave,w.getCurrentWave());
-        // Checking add a new entity
-        Entity e = new EntityImpl(new Vector2D(0, 0));
+    private final WorldFactory worldFactory = new WorldFactoryImpl();
+    private final EntityFactory entityFactory = new EntityFactoryImpl();
+    private  WaveFactory waveFactory;
+    
+    void testBasics(final World w){
+        this.waveFactory = new WaveFactoryImpl(w);
+        var wave = this.waveFactory.createBasicWave(1);
+        var e = this.entityFactory.createBaseEnemy(new Vector2D(0, 0));
+        // Adding one entity
         w.addEntity(e);
         assertTrue(w.getEntities().contains(e));
+        // Removing one entity
+        w.removeEntity(e);
+        assertFalse(w.getEntities().contains(e));
+        // Adding more entities
+        var entities = List.of(
+            this.entityFactory.createBaseEnemy(new Vector2D(0, 0)),
+            this.entityFactory.createBaseEnemy(new Vector2D(0, 0))
+        );
+        w.addEntities(entities);
+        assertTrue(w.getEntities().containsAll(entities));
+        // Removing more entities
+        w.removeEntities(entities);
+        assertFalse(w.getEntities().containsAll(entities));
+        // Getting the map
+        assertNotNull(w.getMap());
+        // Getting get/set wave
+        assertEquals(Optional.empty(), w.getCurrentWave());
+        w.setWave(wave);
+        assertNotEquals(Optional.empty(), w.getCurrentWave());
     }
 
     @Test
+    void testBasicWorld(){
+        var world = worldFactory.createBasicWorld();
+        testBasics(world);
+    }
+
+    @Test 
     void testWorldWithOnePlayer(){
-        var world = this.worldFactory.createWorldWithOnePlayer();
-        // Check if world contains only one player
-        assertTrue(world.getPlayers().size() == 1);
-        // Check the basics
-        basics(world);
-      
+        var world = worldFactory.createWorldWithOnePlayer();
+        assertEquals(1, world.getPlayers().size());
+        testBasics(world);
     }
 
     @Test
-    void testWorldWithMorePlayer(){
-        var players = List.of(this.entityFactory.createPlayer(new Vector2D(0, 0)), this.entityFactory.createPlayer(new Vector2D(0, 0)));
+    void testWorldWithMorePlayers(){
+        var players = List.of(
+            this.entityFactory.createPlayer(new Vector2D(0, 0)),
+            this.entityFactory.createPlayer(new Vector2D(0, 0))
+        );
         var world = this.worldFactory.createWorldWithMorePlayer(players);
-        // Check if world contains more then one players
-        assertTrue(world.getPlayers().size() > 1);
-        // Check the basics
-        basics(world);
+        assertEquals(players.size(), world.getPlayers().size());
+        testBasics(world);
     }
+
+
+
+
 }
