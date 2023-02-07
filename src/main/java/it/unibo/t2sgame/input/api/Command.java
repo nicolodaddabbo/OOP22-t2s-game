f18e61999660c00583d3c8360b3806b75bc4d13e
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 17:51:59 +0100

    [STYLE] Added period to the end of sentences

--- src/main/java/it/unibo/t2sgame/input/api/Command.java
+++ src/main/java/it/unibo/t2sgame/input/api/Command.java
@@ -3,12 +3,13 @@ package it.unibo.t2sgame.input.api;
 import it.unibo.t2sgame.core.entity.api.Entity;
 
 /**
- * This functional interface represent a game Command
+ * This functional interface represent a game Command.
  */
 @FunctionalInterface
 public interface Command {
     /**
-     * Execute the command
+     * Execute the command.
+     * 
      * @param entity to execute the command on
      */
     void execute(Entity entity);
