a8229e9d862bdfc601235486d995fd7698338dc7
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-07 20:11:09 +0100

    [STYLE] Made style changes to rectangle and circle

--- src/main/java/it/unibo/t2sgame/common/shapes/Circle.java
+++ src/main/java/it/unibo/t2sgame/common/shapes/Circle.java
@@ -3,6 +3,9 @@ package it.unibo.t2sgame.common.shapes;
 import it.unibo.t2sgame.common.Shape;
 import it.unibo.t2sgame.common.Vector2D;
 
+/**
+ * This class represents the circle shape.
+ */
 public class Circle implements Shape {
     private Vector2D center;
     private double radius;
@@ -13,15 +16,21 @@ public class Circle implements Shape {
      * @param radius the radius of the circle
      */
     public Circle(final Vector2D center, final double radius) {
-        this.center = center;
-        this.radius = radius;
+        this.setCenter(center);
+        this.setRadius(radius);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Vector2D getCenter() {
         return this.center;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setCenter(final Vector2D center) {
         this.center = center;
@@ -43,19 +52,27 @@ public class Circle implements Shape {
         this.radius = radius;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isColliding(final Shape shape) {
         return shape.isColliding(this);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isColliding(final Circle circle) {
-        return this.center.distance(circle.getCenter()) < this.radius+circle.getRadius();
+        return this.center.distance(circle.getCenter()) < this.radius + circle.getRadius();
     }
 
     /**
-     * @see <a>The formula to check intersection has been taken from 
-     * https://yal.cc/rectangle-circle-intersection-test/comment-page-1/</a>
+     * {@inheritDoc}
+     * 
+     * @see <a>The formula to check intersection has been taken from
+     *      https://yal.cc/rectangle-circle-intersection-test/comment-page-1/</a>
      */
     @Override
     public boolean isColliding(final Rectangle rectangle) {
@@ -65,8 +82,10 @@ public class Circle implements Shape {
         return Math.pow(deltaX, 2) + Math.pow(deltaY, 2) < Math.pow(this.radius, 2);
     }
 
-    private double calcDelta(final double circleCenterAxis, final double rectangleCenterAxis, final double rectangleSide){
-        return circleCenterAxis - Math.max(rectangleCenterAxis-rectangleSide/2, Math.min(circleCenterAxis, rectangleCenterAxis + rectangleSide/2));
+    private double calcDelta(final double circleCenterAxis, final double rectangleCenterAxis,
+            final double rectangleSide) {
+        return circleCenterAxis - Math.max(rectangleCenterAxis - rectangleSide / 2,
+                Math.min(circleCenterAxis, rectangleCenterAxis + rectangleSide / 2));
     }
-    
+
 }
--- src/main/java/it/unibo/t2sgame/common/shapes/Rectangle.java
+++ src/main/java/it/unibo/t2sgame/common/shapes/Rectangle.java
@@ -3,7 +3,10 @@ package it.unibo.t2sgame.common.shapes;
 import it.unibo.t2sgame.common.Shape;
 import it.unibo.t2sgame.common.Vector2D;
 
-public class Rectangle implements Shape{
+/**
+ * This class represents the rectangle shape.
+ */
+public class Rectangle implements Shape {
     private Vector2D center;
     private double width;
     private double height;
@@ -11,20 +14,26 @@ public class Rectangle implements Shape{
     /**
      * 
      * @param center the center of the rectangle
-     * @param width the width of the rectangle
+     * @param width  the width of the rectangle
      * @param height the height of the rectangle
      */
-    public Rectangle(final Vector2D center, final double width, final double height){
-        this.center = center;
-        this.width = width;
-        this.height = height;
+    public Rectangle(final Vector2D center, final double width, final double height) {
+        this.setCenter(center);
+        this.setWidth(width);
+        this.setHeight(height);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Vector2D getCenter() {
         return this.center;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setCenter(final Vector2D center) {
         this.center = center;
@@ -62,20 +71,30 @@ public class Rectangle implements Shape{
         this.height = height;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isColliding(final Shape shape) {
         return shape.isColliding(this);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isColliding(final Circle circle) {
         return circle.isColliding(this);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isColliding(final Rectangle rectangle) {
-        return Math.abs(this.center.getX()-rectangle.getCenter().getX()) < (this.width + rectangle.getWidth())/2 
-            && Math.abs(this.center.getY()-rectangle.getCenter().getY()) < (this.height + rectangle.getHeight())/2;
+        return Math.abs(this.center.getX() - rectangle.getCenter().getX()) < (this.width + rectangle.getWidth()) / 2
+                &&
+                Math.abs(this.center.getY() - rectangle.getCenter().getY()) < (this.height + rectangle.getHeight()) / 2;
     }
-    
+
 }
