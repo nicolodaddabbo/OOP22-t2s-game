85462eea24d1c6c484943fa3ac172e016daeb63f
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 17:52:55 +0100

    [STYLE] Added period to the end of sentences

--- src/main/java/it/unibo/t2sgame/input/api/InputController.java
+++ src/main/java/it/unibo/t2sgame/input/api/InputController.java
@@ -3,12 +3,13 @@ package it.unibo.t2sgame.input.api;
 import java.util.Queue;
 
 /**
- * This interface represent an InutController that gets notified when an input occours
- * and asociate a Command to that given input
+ * This interface represent an InutController that gets notified when an input
+ * occours and asociate a Command to that given input.
  */
 public interface InputController {
     /**
-     * Return the queue of commands to be executed
+     * Return the queue of commands to be executed.
+     * 
      * @return the commands queue
      */
     Queue<Command> getCommandsQueue();
