01a89d76b425654c40564e4cef2038a5f240aa1d
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 17:59:08 +0100

    [STYLE] Added period to the end of sentences

--- src/main/java/it/unibo/t2sgame/game/logics/api/Event.java
+++ src/main/java/it/unibo/t2sgame/game/logics/api/Event.java
@@ -3,12 +3,13 @@ package it.unibo.t2sgame.game.logics.api;
 import it.unibo.t2sgame.game.model.api.World;
 
 /**
- * This functional interface abstract the concept of a game 'event'
+ * This functional interface abstract the concept of a game 'event'.
  */
 @FunctionalInterface
 public interface Event {
     /**
-     * Execute the event
+     * Execute the event.
+     * 
      * @param world where the event has to be executed
      */
     void execute(World world);
