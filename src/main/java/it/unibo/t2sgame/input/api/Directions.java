faadeeefba57c778bb2ba25cdcec0421845c869a
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 16:48:36 +0100

    [DOCS] Added JavaDoc comments to Direction enum

--- src/main/java/it/unibo/t2sgame/input/api/Directions.java
+++ src/main/java/it/unibo/t2sgame/input/api/Directions.java
@@ -1,9 +1,27 @@
 package it.unibo.t2sgame.input.api;
 
+/**
+ * Possible entity's movement directions.
+ */
 public enum Directions {
+    /**
+     * Up direction.
+     */
     UP,
+    /**
+     * Right direction.
+     */
     RIGHT,
+    /**
+     * Down direction.
+     */
     DOWN,
+    /**
+     * Left direction.
+     */
     LEFT,
+    /**
+     * Stay still direction.
+     */
     STAY
 }
