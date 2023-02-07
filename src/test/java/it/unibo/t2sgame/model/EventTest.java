ac3f3bb75bbb7aab6daadfc5740bf4fea9cd9b0d
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 15:34:31 +0100

    [TEST] Added onPowerUp event test

--- src/test/java/it/unibo/t2sgame/model/EventTest.java
+++ src/test/java/it/unibo/t2sgame/model/EventTest.java
@@ -1,5 +1,6 @@
 package it.unibo.t2sgame.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -9,9 +10,12 @@ import it.unibo.t2sgame.common.Vector2D;
 import it.unibo.t2sgame.core.entity.api.Entity;
 import it.unibo.t2sgame.core.entity.api.Type;
 import it.unibo.t2sgame.core.entity.impl.EntityImpl;
+import it.unibo.t2sgame.game.components.ShootComponent;
 import it.unibo.t2sgame.game.logics.api.EventFactory;
 import it.unibo.t2sgame.game.logics.impl.EventFactoryImpl;
 import it.unibo.t2sgame.game.model.api.EntityFactory;
+import it.unibo.t2sgame.game.model.api.PowerUpFactory;
+import it.unibo.t2sgame.game.model.impl.PowerUpFactoryImpl;
 import it.unibo.t2sgame.game.model.api.World;
 import it.unibo.t2sgame.game.model.api.WorldFactory;
 import it.unibo.t2sgame.game.model.impl.EntityFactoryImpl;
@@ -22,11 +26,14 @@ public class EventTest {
     WorldFactory worldFactory = new WorldFactoryImpl();
     World world = worldFactory.createWorldWithOnePlayer();
     EventFactory eventFactory = new EventFactoryImpl();
-    Entity player = new EntityImpl(new Vector2D(0, 0), Type.PLAYER);
-    Entity enemy = new EntityImpl(new Vector2D(0, 0), Type.ENEMY);
     EntityFactory entityFactory = new EntityFactoryImpl();
+    Entity player;
+    Entity enemy = new EntityImpl(new Vector2D(0, 0), Type.ENEMY);
+    PowerUpFactory powerUpFactory = new PowerUpFactoryImpl();
 
     public EventTest() {
+        this.player = this.entityFactory.createPlayer(new Vector2D(0, 0));
+        this.player.setWorld(this.world);
         this.world.addEntity(player);
         this.world.addEntity(enemy);
     }
@@ -46,8 +53,20 @@ public class EventTest {
         this.world.notifyEvent(this.eventFactory.onDeathEvent(enemy));
         // The enemy should still exist after the notification of his death
         assertTrue(this.world.getEntities().stream().filter(e -> e.getType() == Type.ENEMY).findFirst().isPresent());
-        // The world run the onDeath event that removes the death entities when the handleEvents method get called
+        // The world runs the onDeath event that removes the death entities when the handleEvents method get called
         this.world.handleEvents();
         assertFalse(this.world.getEntities().stream().filter(e -> e.getType() == Type.ENEMY).findFirst().isPresent());
+    }
+
+    @Test
+    void onPowerUpEventTest() {
+        var playerShootComponent = this.player.getComponent(ShootComponent.class).get();
+        var initialDamage = playerShootComponent.getProjectileDamage();
+        this.world.notifyEvent(this.eventFactory.onPowerUpEvent(this.powerUpFactory.generateDamageBoostPowerUp()));
+        // The player projectile damage should be the same after the notification of the power up
+        assertEquals(initialDamage, playerShootComponent.getProjectileDamage());
+        // The world runs the onPowerUp event that gives the power up to the player when the handleEvents method get called
+        this.world.handleEvents();
+        assertTrue(playerShootComponent.getProjectileDamage() > initialDamage);
     }
 }
