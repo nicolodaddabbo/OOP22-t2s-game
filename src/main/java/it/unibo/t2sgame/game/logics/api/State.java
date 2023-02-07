7afd8c0d97693ce2d91c103ebbb0b2398ffd3ad8
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-07 16:07:40 +0100

    [STYLE]

--- src/main/java/it/unibo/t2sgame/game/logics/api/State.java
+++ src/main/java/it/unibo/t2sgame/game/logics/api/State.java
@@ -8,31 +8,44 @@ import it.unibo.t2sgame.game.model.api.Wave;
 import it.unibo.t2sgame.game.model.api.World;
 
 /**
- * Rapresent the state and the logics of a Game
+ * Rapresent the state and the logics of a Game.
  */
 public interface State {
     /**
      * 
-     * @return true if logics determines the end of the game, otherwise false
+     * @param players the list of players which are playing the Game.
+     * 
+     * @return true if logics determines the end of the game, otherwise false.
      */
     boolean isOver(List<Entity> players);
+
     /**
      * 
-     * @return true if there aren't survived enemies, otherwise false
+     * @param wave the wave which the status has to check if empty or not.
+     *             This wave to be checked should be the wave linked to the current
+     *             round,
+     *             contained in the World.
+     * 
+     * @return true if there aren't survived enemies, otherwise false.
      */
-    boolean isWaveOver(Optional<Wave> w);
+    boolean isWaveOver(Optional<Wave> wave);
+
     /**
      * 
-     * @return an integer rappresenting the number of the current round
+     * @return an integer rappresenting the number of the current round.
      */
     int getRound();
+
     /**
-     * Increment the number of round depends on the implementation
+     * Increment the number of round.
+     * The incrementing depends on the State's implementation.
      */
     void incrementRound();
+
     /**
-     * Generate the onPowerUp event if a power up can be issued
-     * @param world
+     * Generate the onPowerUp event if a power up can be issued.
+     * 
+     * @param world the world where the event "Power up generated" will be notified
      */
     void generatePowerUp(World world);
 }
--- src/main/java/it/unibo/t2sgame/game/logics/impl/StateImpl.java
+++ src/main/java/it/unibo/t2sgame/game/logics/impl/StateImpl.java
@@ -12,6 +12,10 @@ import it.unibo.t2sgame.game.model.api.Wave;
 import it.unibo.t2sgame.game.model.api.World;
 import it.unibo.t2sgame.game.model.impl.PowerUpFactoryImpl;
 
+/**
+ * This class represents a basics implementation of State's interface.
+ * This class increments the round by 1.
+ */
 public class StateImpl implements State {
     private static final int POWER_UP_ROUND = 5;
     private int round = 0;
@@ -19,31 +23,47 @@ public class StateImpl implements State {
     private final EventFactory eventFactory = new EventFactoryImpl();
     private final Random random = new Random();
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public boolean isOver(List<Entity> players) {
+    public boolean isOver(final List<Entity> players) {
         return players.isEmpty();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public boolean isWaveOver(Optional<Wave> w) {
+    public boolean isWaveOver(final Optional<Wave> w) {
         return w.isEmpty() || w.get().getEnemies().isEmpty();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getRound() {
         return this.round;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void incrementRound() {
         this.round++;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void generatePowerUp(final World world) {
         if (this.round % POWER_UP_ROUND == 0) {
             var powerUpList = this.powerUpFactory.getObtainablePowerUpList();
-            world.notifyEvent(this.eventFactory.onPowerUpEvent(powerUpList.get(this.random.nextInt(powerUpList.size()))));
+            world.notifyEvent(
+                    this.eventFactory.onPowerUpEvent(powerUpList.get(this.random.nextInt(powerUpList.size()))));
         }
     }
 
