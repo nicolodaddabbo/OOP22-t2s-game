4346909513b9dcbde4b1b0f86641b813a41ac494
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 17:43:02 +0100

    [STYLE] Fixed indentation

--- src/main/java/it/unibo/t2sgame/input/impl/MosquitoAIInputController.java
+++ src/main/java/it/unibo/t2sgame/input/impl/MosquitoAIInputController.java
@@ -7,26 +7,29 @@ import it.unibo.t2sgame.input.api.AbstractChasingAIInputController;
 import it.unibo.t2sgame.input.api.Directions;
 
 /**
- * AI that randomly change direction every TIME_TO_NEXT_DECISION ms 
+ * AI that randomly change direction every TIME_TO_NEXT_DECISION ms.
  */
 public class MosquitoAIInputController extends AbstractChasingAIInputController {
     private final Random random = new Random();
     private static final long TIME_TO_NEXT_DECISION = 500;
     private long lastChoiceTime = 0;
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void computeNextCommand() {
         super.addToCommandsQueue(entity -> {
             if (isTimeToChoose()) {
                 entity.notifyComponent(PhysicsComponent.class,
-                    () -> Directions.values()[random.nextInt(Directions.values().length)]);
+                        () -> Directions.values()[random.nextInt(Directions.values().length)]);
                 this.lastChoiceTime = System.currentTimeMillis();
             }
         });
     }
 
-    private boolean isTimeToChoose(){
-		return System.currentTimeMillis() - lastChoiceTime > TIME_TO_NEXT_DECISION;
-	}
+    private boolean isTimeToChoose() {
+        return System.currentTimeMillis() - lastChoiceTime > TIME_TO_NEXT_DECISION;
+    }
 
 }
