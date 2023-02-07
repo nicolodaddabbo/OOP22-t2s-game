0c2b427ec2b8e2e487c9b728f9eb42151a221882
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 16:40:55 +0100

    [STYLE] Corrected JavaDocs comments styles and removed magic numbers

--- src/main/java/it/unibo/t2sgame/input/impl/GaussianAIInputController.java
+++ src/main/java/it/unibo/t2sgame/input/impl/GaussianAIInputController.java
@@ -9,50 +9,71 @@ import it.unibo.t2sgame.input.api.AbstractChasingAIInputController;
 import it.unibo.t2sgame.input.api.Directions;
 
 /**
- * AI that uses Gaussian distribution to generate random movements for an entity, 
- * with the aim of gradually approaching the adversary's position.
+ * AI that uses Gaussian distribution to generate random movements for an
+ * entity, with the aim of gradually approaching the adversary's position.
  */
 public class GaussianAIInputController extends AbstractChasingAIInputController {
-    final Random random = new Random();
+    private final Random random = new Random();
     private static final long TIME_TO_NEXT_DECISION = 300;
     private static final long STANDARD_DEVIATION = 500;
+    private static final double TWO_PI = 2 * Math.PI;
+    // Factor used to adjust the scale of the normally distributed random variable
+    private static final double SCALE_FACTOR = -2;
     private long lastChoiceTime = 0;
     private final Type adversaryType;
 
+    /**
+     * Gaussian AI InputController constructor.
+     * 
+     * @param adversaryType type of the adversary to chase
+     */
     public GaussianAIInputController(final Type adversaryType) {
         this.adversaryType = adversaryType;
     }
 
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     protected void computeNextCommand() {
         this.commandsQueue.add(entity -> {
             if (isTimeToChoose()) {
                 var closestAdeversary = super.findClosestAdeversary(entity, this.adversaryType);
                 closestAdeversary.ifPresent(a -> entity.notifyComponent(PhysicsComponent.class,
-                        () -> generateGaussianRandomDirection(entity.getPosition(), closestAdeversary.get().getPosition(), STANDARD_DEVIATION)));
+                        () -> generateGaussianRandomDirection(entity.getPosition(),
+                                closestAdeversary.get().getPosition(), STANDARD_DEVIATION)));
                 this.lastChoiceTime = System.currentTimeMillis();
             }
         });
     }
 
-    private Directions generateGaussianRandomDirection(final Vector2D currentEntityPos, final Vector2D playerPos, final double standardDeviation) {
-        // generate 2 random numbers, u1 and u2, from a uniform distribution between 0
-        // and 1
+    /**
+     * This method uses Box-Muller transform to generate a pair of indepented normally distributed 
+     * random numbers then scales them using the adversary's position.
+     * @param currentEntityPosition
+     * @param adversaryPosition
+     * @param standardDeviation
+     * @return a Gaussian distributed random direction
+     */
+    private Directions generateGaussianRandomDirection(final Vector2D currentEntityPosition, final Vector2D adversaryPosition,
+            final double standardDeviation) {
+        // Generate 2 random numbers, u1 and u2, from a uniform distribution between 0 and 1
         var u1 = this.random.nextDouble();
         var u2 = this.random.nextDouble();
-        // this equation generate a STANDARD normally distributed random variable for
-        // the x-coordinate
-        var standardX = Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2);
+        // This equation generate a STANDARD normally distributed random variable for the x-coordinate
+        var standardX = Math.sqrt(SCALE_FACTOR * Math.log(u1)) * Math.cos(TWO_PI * u2);
         // and this generate it for the y-coordinate
-        var standardY = Math.sqrt(-2 * Math.log(u1)) * Math.sin(2 * Math.PI * u2);
-        // this equation scale the standard normal random variable, using the player
-        // x-coordinate as the mean,
-        // creating a normal random variable centered on the player x-coordinate
-        var randomX = playerPos.getX() + standardDeviation * standardX;
-        // and this is centered on the entity y-coordinate
-        var randomY = playerPos.getY() + standardDeviation * standardY;
-        var dX = randomX - currentEntityPos.getX();
-        var dY = randomY - currentEntityPos.getY();
+        var standardY = Math.sqrt(SCALE_FACTOR * Math.log(u1)) * Math.sin(TWO_PI * u2);
+        /*
+         * This equation scale the standard normal random variable, using 
+         * the player x-coordinate as the mean, creating a normal random variable 
+         * centered on the adversary's x-coordinate.
+         */
+        var randomX = adversaryPosition.getX() + standardDeviation * standardX;
+        // And this is centered on the adversary's y-coordinate
+        var randomY = adversaryPosition.getY() + standardDeviation * standardY;
+        var dX = randomX - currentEntityPosition.getX();
+        var dY = randomY - currentEntityPosition.getY();
         var angle = Math.toDegrees(Math.atan2(dY, dX));
         return super.findDirectionGivenAngle(angle);
     }
