afe805e73c08a1b183d547382da8a6fb5b9872af
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 17:45:01 +0100

    [DOCS] Added Javadoc comment for @param <I> tag

--- src/main/java/it/unibo/t2sgame/input/api/EntityState.java
+++ src/main/java/it/unibo/t2sgame/input/api/EntityState.java
@@ -3,18 +3,22 @@ package it.unibo.t2sgame.input.api;
 import java.util.Optional;
 
 /**
- * Interface that model the state (i.e. Concurrent State Machine) of an entity
+ * Interface that model the state (i.e. Concurrent State Machine) of an entity.
+ * 
+ * @param <I> input type
  */
 public interface EntityState<I> {
     /**
-     * Change the currentCommand of the state based on the input received
+     * Change the currentCommand of the state based on the input received.
+     * 
      * @param input
      */
     void notifyInput(I input);
 
     /**
-     * Change the currentCommand of the state based on the release of the input received
-     * @param input released
+     * Change the currentCommand of the state based on the release of the input received.
+     * 
+     * @param input               released
      * @param releaseCommandValue optional of the command to execute when the input is released
      */
     void notifyInputRelease(I input, Optional<Command> releaseCommandValue);
