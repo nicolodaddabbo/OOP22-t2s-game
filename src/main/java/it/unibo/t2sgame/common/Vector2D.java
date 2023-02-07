f4eeff12e100459f3cfcc7aca439f0281e99aae8
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-07 21:04:24 +0100

    [STYLE] Made style changes to Vector2D equals method

--- src/main/java/it/unibo/t2sgame/common/Vector2D.java
+++ src/main/java/it/unibo/t2sgame/common/Vector2D.java
@@ -119,6 +119,9 @@ public class Vector2D {
         return this.distance(point.x, point.y);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -131,20 +134,26 @@ public class Vector2D {
         return result;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(final Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (this.getClass() != obj.getClass()) {
             return false;
+        }
         Vector2D other = (Vector2D) obj;
-        if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
-            return false;
-        if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
-            return false;
-        return true;
+        return axisEquals(this.x, other.x) && axisEquals(this.y, other.y);
+    }
+
+    private boolean axisEquals(final double axis1, final double axis2) {
+        return Double.doubleToLongBits(axis1) != Double.doubleToLongBits(axis2);
     }
 
     /**
