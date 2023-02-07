1e6628bb99ca9d758c875d60903f9f26cc927454
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-07 17:58:18 +0100

    [DOCS] Fixed onShoot Javadoc comment

--- src/main/java/it/unibo/t2sgame/game/logics/api/EventFactory.java
+++ src/main/java/it/unibo/t2sgame/game/logics/api/EventFactory.java
@@ -4,23 +4,22 @@ import it.unibo.t2sgame.core.entity.api.Entity;
 import it.unibo.t2sgame.game.model.api.PowerUp;
 
 /**
- * This interface model an EventFactory
+ * This interface model an EventFactory.
  */
 public interface EventFactory {
     /**
-     * @param shooter the entity that shot the projectile
-     * @param direction the direction of the projectile
+     * @param projectile to shoot
      * @return an onShoot event
      */
-    public Event onShootEvent(Entity projectile);
+    Event onShootEvent(Entity projectile);
     /**
      * @param entity to remove
      * @return an onDeath event
      */
-    public Event onDeathEvent(Entity entity);
+    Event onDeathEvent(Entity entity);
     /**
      * @param powerUp to apply
      * @return an onPowerUp event
      */
-    public Event onPowerUpEvent(PowerUp powerUp);
+    Event onPowerUpEvent(PowerUp powerUp);
 }
