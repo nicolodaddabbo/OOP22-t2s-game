c14c4ff640994f2a3c4a4d52fc95c9a5f27dbd33
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-02-08 10:53:51 +0100

    [REFACTOR] changed way random is handled now the enemies have a probabilistic weigh

--- src/main/java/it/unibo/t2sgame/game/model/impl/WaveFactoryImpl.java
+++ src/main/java/it/unibo/t2sgame/game/model/impl/WaveFactoryImpl.java
@@ -12,13 +12,16 @@ import it.unibo.t2sgame.game.model.api.EntityFactory;
 import it.unibo.t2sgame.game.model.api.Wave;
 import it.unibo.t2sgame.game.model.api.WaveFactory;
 import it.unibo.t2sgame.game.model.api.World;
-
+/**
+ * class that is implementing interface WaveFactory.
+ */
 public class WaveFactoryImpl implements WaveFactory {
     private EntityFactory entityFactory = new EntityFactoryImpl();
     private Random random = new Random();
     private World world;
     private final double width;
     private final double height;
+    private static final double OUTSIDEMAPVALUE = 100.0;
 
     private enum Enemy {
         BASE(EntityFactory::createBaseEnemy),
@@ -26,23 +29,33 @@ public class WaveFactoryImpl implements WaveFactory {
         WILD(EntityFactory::createWildEnemy),
         BOSS(EntityFactory::createBossEnemy);
 
-        BiFunction<EntityFactory, Vector2D, Entity> entityBiFunction;
+        private BiFunction<EntityFactory, Vector2D, Entity> entityBiFunction;
 
-        Enemy(BiFunction<EntityFactory, Vector2D, Entity> entityBiFunction) {
+        Enemy(final BiFunction<EntityFactory, Vector2D, Entity> entityBiFunction) {
             this.entityBiFunction = entityBiFunction;
         }
 
         private static final Random RANDOM = new Random();
+        private static final int BASEENEMYPROBABILISTICWEIGH = 50;
+        private static final int GAUSSIANENEMYPROBABILISTICWEIGH = 90;
 
         public static Enemy randomEnemy() {
-            return Enemy.values()[RANDOM.nextInt(Enemy.values().length - 1)];
+            var randomValue = RANDOM.nextInt(100);
+            return randomValue < BASEENEMYPROBABILISTICWEIGH 
+                ? Enemy.BASE 
+                : randomValue < GAUSSIANENEMYPROBABILISTICWEIGH 
+                    ? Enemy.GAUSSIAN 
+                    : Enemy.WILD;
         }
 
         public BiFunction<EntityFactory, Vector2D, Entity> getBiFunction() {
             return this.entityBiFunction;
         }
     }
-
+    /**
+     * constructor of WaveFactoryImpl.
+     * @param world in which the waves are going to be spawned
+     */
     public WaveFactoryImpl(final World world) {
         this.world = world;
         this.width = this.world.getMap().getWidth();
@@ -51,26 +64,34 @@ public class WaveFactoryImpl implements WaveFactory {
 
     private Wave createWaveFromEnemies(final List<Entity> enemies) {
         return new Wave() {
-            final int roundId = enemies.size();
-
+            private final int roundId = enemies.size();
+            /**
+            * {@inheritDoc}
+            */
             @Override
             public List<Entity> getEnemies() {
                 return enemies;
             }
-
+            /**
+            * {@inheritDoc}
+            */
             @Override
             public int getWaveID() {
                 return this.roundId;
             }
-
+            /**
+            * {@inheritDoc}
+            */
             @Override
-            public Wave addEnemy(Entity entity) {
+            public Wave addEnemy(final Entity entity) {
                 enemies.add(entity);
                 return this;
             }
         };
     }
-
+    /**
+    * {@inheritDoc}
+    */
     @Override
     public Wave createBasicWave(final int round) {
         return createWaveFromEnemies(Stream.generate(() -> Enemy.BASE.getBiFunction().apply(this.entityFactory,
@@ -78,15 +99,20 @@ public class WaveFactoryImpl implements WaveFactory {
                 .limit(round)
                 .collect(Collectors.toList()));
     }
-
+    /**
+    * {@inheritDoc}
+    */
     @Override
     public Wave createRandomWave(final int round) {
         return createWaveFromEnemies(Stream.generate(() -> Enemy.randomEnemy().getBiFunction().apply(entityFactory,
-                    new Vector2D(random.nextDouble(0, this.width), random.nextDouble(0, this.height))))
-                .limit(round)
+                    new Vector2D(random.nextDouble(-OUTSIDEMAPVALUE, this.width + OUTSIDEMAPVALUE), 
+                                random.nextDouble(-OUTSIDEMAPVALUE, this.height + OUTSIDEMAPVALUE))))
+                .limit((long) Math.ceil(round / 2.0))
                 .collect(Collectors.toList()));
     }
-
+    /**
+    * {@inheritDoc}
+    */
     @Override
     public Wave createBossWave(final int round) {
         return this.createRandomWave(round / 2)
