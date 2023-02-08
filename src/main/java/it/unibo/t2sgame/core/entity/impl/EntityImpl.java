617ca97cabf2c31f6130e035ce5dc4d134bdcfaa
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-08 10:33:19 +0100

    [STYLE] Made quality of code changes to entity itnerface and implementation

--- src/main/java/it/unibo/t2sgame/core/entity/impl/EntityImpl.java
+++ src/main/java/it/unibo/t2sgame/core/entity/impl/EntityImpl.java
@@ -1,4 +1,5 @@
 package it.unibo.t2sgame.core.entity.impl;
+
 import it.unibo.t2sgame.common.Vector2D;
 import it.unibo.t2sgame.core.components.api.Component;
 import it.unibo.t2sgame.core.components.api.Message;
@@ -10,6 +11,9 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * This class represents an entity.
+ */
 public class EntityImpl implements Entity {
 
     private Set<Component> components = new HashSet<>();
@@ -17,24 +21,38 @@ public class EntityImpl implements Entity {
     private final Type type;
     private Vector2D position;
 
+    /**
+     * 
+     * @param position the starting position of the entity
+     * @param type     the type of the entity
+     */
     public EntityImpl(final Vector2D position, final Type type) {
         this.position = position;
         this.type = type;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Set<Component> getComponents() {
         return this.components;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public <T extends Component> Optional<T> getComponent(final Class<T> componentClass){
+    public <T extends Component> Optional<T> getComponent(final Class<T> componentClass) {
         return this.components.stream()
-            .filter(componentClass::isInstance)
-            .map(componentClass::cast)
-            .findFirst();
+                .filter(componentClass::isInstance)
+                .map(componentClass::cast)
+                .findFirst();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Entity addComponent(final Component component) {
         this.components.add(component);
@@ -42,31 +60,49 @@ public class EntityImpl implements Entity {
         return this;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Vector2D getPosition() {
         return this.position;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setPosition(final Vector2D position) {
         this.position = position;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Type getType() {
         return this.type;
-    }  
+    }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Optional<World> getWorld() {
         return this.world;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setWorld(final World world) {
-        this.world = Optional.of(world);        
+        this.world = Optional.of(world);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public <T extends Component, S> void notifyComponent(final Class<T> receiver, final Message<S> message) {
         this.getComponent(receiver).ifPresent(c -> c.receive(message));
