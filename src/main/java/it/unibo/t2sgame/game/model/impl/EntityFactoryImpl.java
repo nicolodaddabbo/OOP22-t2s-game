4434cf024ac2cfdc79a33379082760e417c82157
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-09 10:48:41 +0100

    [STYLE] Made quality of code changes in EntityFactory

--- src/main/java/it/unibo/t2sgame/game/model/impl/EntityFactoryImpl.java
+++ src/main/java/it/unibo/t2sgame/game/model/impl/EntityFactoryImpl.java
@@ -7,14 +7,10 @@ import it.unibo.t2sgame.common.Vector2D;
 import it.unibo.t2sgame.common.shapes.Circle;
 import it.unibo.t2sgame.common.shapes.Rectangle;
 import it.unibo.t2sgame.core.components.api.ComponentFactory;
-import it.unibo.t2sgame.core.components.impl.CollisionComponent;
 import it.unibo.t2sgame.core.components.impl.ComponentFactoryImpl;
 import it.unibo.t2sgame.core.entity.api.Entity;
 import it.unibo.t2sgame.core.entity.api.Type;
 import it.unibo.t2sgame.core.entity.impl.EntityImpl;
-import it.unibo.t2sgame.game.components.BaseCollisionComponent;
-import it.unibo.t2sgame.game.components.HealthComponent;
-import it.unibo.t2sgame.game.components.ShootComponent;
 import it.unibo.t2sgame.game.model.api.EntityFactory;
 import it.unibo.t2sgame.input.api.Directions;
 import it.unibo.t2sgame.input.impl.ChasingAIInputController;
@@ -26,16 +22,9 @@ import it.unibo.t2sgame.input.impl.KeyboardInputController;
  * entities.
  */
 public class EntityFactoryImpl implements EntityFactory {
-    private static final int BOSS_ENEMY_HEALTH = 25;
-    private static final int BOSS_ENEMY_DAMAGE = 2;
-    private static final double BOSS_ENEMY_SPEED = 0.3;
-    private static final double BOSS_ENEMY_COLLISION_WIDTH = 200.0;
-    private static final double BOSS_ENEMY_COLLISION_HEIGHT = 140.0;
-    private static final double BOSS_ENEMY_RENDER_WIDTH = 200.0;
-    private static final double BOSS_ENEMY_RENDER_HEIGHT = 140.0;
     private final ComponentFactory componentFactory = new ComponentFactoryImpl();
     private final Random random = new Random();
-        
+
     /**
      * {@inheritDoc}
      */
@@ -44,11 +33,15 @@ public class EntityFactoryImpl implements EntityFactory {
         return new EntityImpl(position, Type.PLAYER)
                 .addComponent(this.componentFactory.createInputComponentFrom(new KeyboardInputController()))
                 .addComponent(this.componentFactory.createPhysicsComponentFrom(Stats.Player.MOVEMENT_SPEED))
-                .addComponent(this.componentFactory.createCollisionComponentFrom(new Rectangle(position, Stats.Player.COLLISION_WIDTH, Stats.Player.COLLISION_HEIGHT), false, List.of(Type.ENEMY, Type.WALL)))
+                .addComponent(this.componentFactory.createCollisionComponentFrom(
+                        new Rectangle(position, Stats.Player.COLLISION_WIDTH, Stats.Player.COLLISION_HEIGHT), false,
+                        List.of(Type.ENEMY, Type.WALL)))
                 .addComponent(this.componentFactory.createHealthComponentFrom(Stats.Player.HEALTH))
-                .addComponent(this.componentFactory.createShootComponentFrom(Stats.Player.FIRERATE_SECONDS, Stats.Player.PROJECTILE_SPEED, Stats.Player.PROJECTILE_DAMAGE, Stats.Player.PROJECTILE_SIZE))
+                .addComponent(this.componentFactory.createShootComponentFrom(Stats.Player.FIRERATE_SECONDS,
+                        Stats.Player.PROJECTILE_SPEED, Stats.Player.PROJECTILE_DAMAGE, Stats.Player.PROJECTILE_SIZE))
                 .addComponent(
-                        this.componentFactory.createGraphicComponentWithSprite("player", Stats.Player.RENDER_WIDTH, Stats.Player.RENDER_HEIGHT));
+                        this.componentFactory.createGraphicComponentWithSprite("player", Stats.Player.RENDER_WIDTH,
+                                Stats.Player.RENDER_HEIGHT));
     }
 
     /**
@@ -60,10 +53,13 @@ public class EntityFactoryImpl implements EntityFactory {
                 .addComponent(this.componentFactory.createInputComponentFrom(new ChasingAIInputController(Type.ENEMY)))
                 .addComponent(this.componentFactory.createPhysicsComponentFrom(Stats.Companion.MOVEMENT_SPEED))
                 .addComponent(this.componentFactory.createCollisionComponentFrom(
-                        new Rectangle(position, Stats.Companion.COLLISION_WIDTH, Stats.Companion.COLLISION_HEIGHT), false, List.of(Type.WALL)))
-                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.Companion.DAMAGE, Stats.Companion.COOLDOWN_SECONDS))
+                        new Rectangle(position, Stats.Companion.COLLISION_WIDTH, Stats.Companion.COLLISION_HEIGHT),
+                        false, List.of(Type.WALL)))
+                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.Companion.DAMAGE,
+                        Stats.Companion.COOLDOWN_SECONDS))
                 .addComponent(
-                        this.componentFactory.createGraphicComponentWithSprite("companion", Stats.Companion.RENDER_WIDTH, Stats.Companion.RENDER_HEIGHT));
+                        this.componentFactory.createGraphicComponentWithSprite("companion",
+                                Stats.Companion.RENDER_WIDTH, Stats.Companion.RENDER_HEIGHT));
     }
 
     /**
@@ -79,7 +75,7 @@ public class EntityFactoryImpl implements EntityFactory {
                 .addComponent(this.componentFactory.createDamageComponentFrom(damage, 0))
                 .addComponent(this.componentFactory.createCircleGraphicComponent(size * 2, size * 2));
     }
-        
+
     /**
      * {@inheritDoc}
      */
@@ -92,9 +88,11 @@ public class EntityFactoryImpl implements EntityFactory {
                 .addComponent(this.componentFactory.createCollisionComponentFrom(
                         new Rectangle(position, Stats.Enemy.COLLISION_WIDTH, Stats.Enemy.COLLISION_HEIGHT),
                         false, List.of(Type.PROJECTILE, Type.COMPANION)))
-                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.Enemy.DAMAGE, Stats.Enemy.DAMAGE_COOLDOWN))
+                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.Enemy.DAMAGE,
+                        Stats.Enemy.DAMAGE_COOLDOWN))
                 .addComponent(
-                        this.componentFactory.createGraphicComponentWithSprite("ice_enemy", Stats.Enemy.RENDER_WIDTH, Stats.Enemy.RENDER_HEIGHT));
+                        this.componentFactory.createGraphicComponentWithSprite("ice_enemy", Stats.Enemy.RENDER_WIDTH,
+                                Stats.Enemy.RENDER_HEIGHT));
     }
 
     /**
@@ -108,11 +106,14 @@ public class EntityFactoryImpl implements EntityFactory {
                 .addComponent(this.componentFactory.createHealthComponentFrom(Stats.GaussianEnemy.HEALTH))
                 .addComponent(this.componentFactory.createPhysicsComponentFrom(Stats.GaussianEnemy.MOVEMENT_SPEED))
                 .addComponent(this.componentFactory.createCollisionComponentFrom(
-                        new Rectangle(position, Stats.GaussianEnemy.COLLISION_WIDTH, Stats.GaussianEnemy.COLLISION_HEIGHT),
+                        new Rectangle(position, Stats.GaussianEnemy.COLLISION_WIDTH,
+                                Stats.GaussianEnemy.COLLISION_HEIGHT),
                         false, List.of(Type.PROJECTILE, Type.COMPANION)))
-                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.GaussianEnemy.DAMAGE, Stats.GaussianEnemy.DAMAGE_COOLDOWN))
+                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.GaussianEnemy.DAMAGE,
+                        Stats.GaussianEnemy.DAMAGE_COOLDOWN))
                 .addComponent(
-                        this.componentFactory.createGraphicComponentWithSprite("fire_enemy", Stats.GaussianEnemy.RENDER_WIDTH, Stats.GaussianEnemy.RENDER_HEIGHT));
+                        this.componentFactory.createGraphicComponentWithSprite("fire_enemy",
+                                Stats.GaussianEnemy.RENDER_WIDTH, Stats.GaussianEnemy.RENDER_HEIGHT));
     }
 
     /**
@@ -130,9 +131,11 @@ public class EntityFactoryImpl implements EntityFactory {
                 .addComponent(this.componentFactory.createCollisionComponentFrom(
                         new Rectangle(position, Stats.WildEnemy.COLLISION_WIDTH, Stats.WildEnemy.COLLISION_HEIGHT),
                         false, List.of(Type.PROJECTILE, Type.COMPANION)))
-                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.WildEnemy.DAMAGE, Stats.WildEnemy.DAMAGE_COOLDOWN))
+                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.WildEnemy.DAMAGE,
+                        Stats.WildEnemy.DAMAGE_COOLDOWN))
                 .addComponent(
-                        this.componentFactory.createGraphicComponentWithSprite("rainbow_enemy", Stats.WildEnemy.RENDER_WIDTH, Stats.WildEnemy.RENDER_HEIGHT));
+                        this.componentFactory.createGraphicComponentWithSprite("rainbow_enemy",
+                                Stats.WildEnemy.RENDER_WIDTH, Stats.WildEnemy.RENDER_HEIGHT));
     }
 
     /**
@@ -147,9 +150,11 @@ public class EntityFactoryImpl implements EntityFactory {
                 .addComponent(this.componentFactory.createCollisionComponentFrom(
                         new Rectangle(position, Stats.BossEnemy.COLLISION_WIDTH, Stats.BossEnemy.COLLISION_HEIGHT),
                         false, List.of(Type.PROJECTILE, Type.COMPANION)))
-                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.BossEnemy.DAMAGE, Stats.BossEnemy.DAMAGE_COOLDOWN))
+                .addComponent(this.componentFactory.createDamageComponentFrom(Stats.BossEnemy.DAMAGE,
+                        Stats.BossEnemy.DAMAGE_COOLDOWN))
                 .addComponent(
-                        this.componentFactory.createGraphicComponentWithSprite("crown_enemy", Stats.BossEnemy.RENDER_WIDTH, Stats.BossEnemy.RENDER_HEIGHT));
+                        this.componentFactory.createGraphicComponentWithSprite("crown_enemy",
+                                Stats.BossEnemy.RENDER_WIDTH, Stats.BossEnemy.RENDER_HEIGHT));
     }
 
     /**
