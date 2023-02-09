13215358b61767d870d8b4e494457f330554dcd9
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-09 10:49:29 +0100

    [REFACTOR] Made stats package level and made Enemy final

--- src/main/java/it/unibo/t2sgame/game/model/impl/Stats.java
+++ src/main/java/it/unibo/t2sgame/game/model/impl/Stats.java
@@ -1,78 +1,97 @@
 package it.unibo.t2sgame.game.model.impl;
 
-import java.util.List;
 import java.util.Random;
 
-import it.unibo.t2sgame.core.entity.api.Type;
+final class Stats {
 
-public final class Stats {
-    
     private Stats() {
     }
 
-    public static final class Player{
+    static final class Player {
         private Player() {
         }
-        public static final int HEALTH = 3;
-        public static final double MOVEMENT_SPEED = 1.0;
-        public static final int PROJECTILE_DAMAGE = 1;
-        public static final double FIRERATE_SECONDS = 0.5;
-        public static final double PROJECTILE_SIZE = 20.0;
-        public static final double PROJECTILE_SPEED = 1.5;
-        public static final double COLLISION_WIDTH = 60.0;
-        public static final double COLLISION_HEIGHT = 80.0;
-        public static final double RENDER_WIDTH = 60.0;
-        public static final double RENDER_HEIGHT = 80.0;
+
+        static final int HEALTH = 3;
+        static final double MOVEMENT_SPEED = 1.0;
+        static final int PROJECTILE_DAMAGE = 1;
+        static final double FIRERATE_SECONDS = 0.5;
+        static final double PROJECTILE_SIZE = 20.0;
+        static final double PROJECTILE_SPEED = 1.5;
+        static final double COLLISION_WIDTH = 60.0;
+        static final double COLLISION_HEIGHT = 80.0;
+        static final double RENDER_WIDTH = 60.0;
+        static final double RENDER_HEIGHT = 80.0;
     }
 
-    public static final class Companion{
+    static final class Companion {
         private Companion() {
         }
-        public static final double MOVEMENT_SPEED = 1.0;
-        public static final int DAMAGE = 1;
-        public static final double COOLDOWN_SECONDS = 1.0;
-        public static final double COLLISION_WIDTH = 60.0;
-        public static final double COLLISION_HEIGHT = 80.0;
-        public static final double RENDER_WIDTH = 60.0;
-        public static final double RENDER_HEIGHT = 80.0;
+
+        static final double MOVEMENT_SPEED = 1.0;
+        static final int DAMAGE = 1;
+        static final double COOLDOWN_SECONDS = 1.0;
+        static final double COLLISION_WIDTH = 60.0;
+        static final double COLLISION_HEIGHT = 80.0;
+        static final double RENDER_WIDTH = 60.0;
+        static final double RENDER_HEIGHT = 80.0;
     }
 
-    public static class Enemy{
+    static final class Enemy {
         private Enemy() {
         }
-        public static final int HEALTH = 1;
-        public static final int DAMAGE = 1;
-        public static final double DAMAGE_COOLDOWN = 1.0;
-        public static final double MOVEMENT_SPEED = 0.3;
-        public static final double COLLISION_WIDTH = 60.0;
-        public static final double COLLISION_HEIGHT = 50.0;
-        public static final double RENDER_WIDTH = 60.0;
-        public static final double RENDER_HEIGHT = 60.0;
+
+        static final int HEALTH = 1;
+        static final int DAMAGE = 1;
+        static final double DAMAGE_COOLDOWN = 1.0;
+        static final double MOVEMENT_SPEED = 0.3;
+        static final double COLLISION_WIDTH = 60.0;
+        static final double COLLISION_HEIGHT = 50.0;
+        static final double RENDER_WIDTH = 60.0;
+        static final double RENDER_HEIGHT = 60.0;
     }
 
-    public static final class GaussianEnemy extends Enemy{
-        private static final Random random = new Random();
+    static final class GaussianEnemy {
         private GaussianEnemy() {
         }
-        public static final double MOVEMENT_SPEED = random.nextDouble(0.3, 1);
+
+        static final int HEALTH = 1;
+        static final int DAMAGE = 1;
+        static final double DAMAGE_COOLDOWN = 1.0;
+        static final double MOVEMENT_SPEED = 0.8;
+        static final double COLLISION_WIDTH = 60.0;
+        static final double COLLISION_HEIGHT = 50.0;
+        static final double RENDER_WIDTH = 60.0;
+        static final double RENDER_HEIGHT = 60.0;
     }
 
-    public static final class WildEnemy extends Enemy{
+    static final class WildEnemy {
+        private static final Random RANDOM = new Random();
+
         private WildEnemy() {
         }
-        public static final double MOVEMENT_SPEED = 0.8;
+
+        static final int HEALTH = 1;
+        static final int DAMAGE = 1;
+        static final double DAMAGE_COOLDOWN = 1.0;
+        static final double MOVEMENT_SPEED = RANDOM.nextDouble(0.3, 1);
+        static final double COLLISION_WIDTH = 60.0;
+        static final double COLLISION_HEIGHT = 50.0;
+        static final double RENDER_WIDTH = 60.0;
+        static final double RENDER_HEIGHT = 60.0;
     }
 
-    public static final class BossEnemy extends Enemy{
+    static final class BossEnemy {
         private BossEnemy() {
         }
-        public static final int HEALTH = 25;
-        public static final int DAMAGE = 2;
-        public static final double MOVEMENT_SPEED = 0.3;
-        public static final double COLLISION_WIDTH = 200.0;
-        public static final double COLLISION_HEIGHT = 140.0;
-        public static final double RENDER_WIDTH = 200.0;
-        public static final double RENDER_HEIGHT = 140.0;
+
+        static final int HEALTH = 25;
+        static final int DAMAGE = 2;
+        static final double DAMAGE_COOLDOWN = 1.0;
+        static final double MOVEMENT_SPEED = 0.3;
+        static final double COLLISION_WIDTH = 200.0;
+        static final double COLLISION_HEIGHT = 140.0;
+        static final double RENDER_WIDTH = 200.0;
+        static final double RENDER_HEIGHT = 140.0;
     }
 
 }
