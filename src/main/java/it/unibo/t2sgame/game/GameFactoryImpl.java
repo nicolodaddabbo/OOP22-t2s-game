90fbe153f6a07c8eae18ac176e0e934ae18904ed
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-09 18:43:21 +0100

    [FIX] substitute createWorldWithMorePlayers with createWorldWithPlayerAndCompanion

--- src/main/java/it/unibo/t2sgame/game/GameFactoryImpl.java
+++ src/main/java/it/unibo/t2sgame/game/GameFactoryImpl.java
@@ -1,29 +1,30 @@
 package it.unibo.t2sgame.game;
 
-import java.util.List;
-
-import it.unibo.t2sgame.common.Vector2D;
 import it.unibo.t2sgame.game.logics.impl.StateImpl;
-import it.unibo.t2sgame.game.model.api.EntityFactory;
 import it.unibo.t2sgame.game.model.api.WorldFactory;
-import it.unibo.t2sgame.game.model.impl.EntityFactoryImpl;
 import it.unibo.t2sgame.game.model.impl.WorldFactoryImpl;
 
+/**
+ * A GameFactory's implementation.
+ * See {@link GameFactory}
+ */
 public class GameFactoryImpl implements GameFactory {
-    private final EntityFactory entityFactory = new EntityFactoryImpl();
+
     private final WorldFactory worldFactory = new WorldFactoryImpl();
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Game createSinglePlayerGame() {
-        return new GameImpl(new StateImpl(), this.worldFactory.createWorldWithOnePlayer());
+        return new T2SGame(new StateImpl(), this.worldFactory.createWorldWithOnePlayer());
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Game createMultiPlayerGame() {
-        return new GameImpl(new StateImpl(), this.worldFactory.createWorldWithMorePlayer(
-                List.of(
-                        this.entityFactory.createPlayer(new Vector2D(0, 0)),
-                        this.entityFactory.createPlayer(new Vector2D(0, 0)))));
+        return new T2SGame(new StateImpl(), this.worldFactory.createWorldWithPlayerAndCompanion());
     }
-
 }
