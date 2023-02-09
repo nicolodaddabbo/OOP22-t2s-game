14037c8e3628f46c3e4a76485bb7f6f7d5493222
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-09 18:41:06 +0100

    [STYLE] arranged style

--- src/main/java/it/unibo/t2sgame/game/Game.java
+++ src/main/java/it/unibo/t2sgame/game/Game.java
@@ -1,30 +1,33 @@
 package it.unibo.t2sgame.game;
 
-import it.unibo.t2sgame.game.logics.api.GameMap;
 import it.unibo.t2sgame.game.logics.api.State;
 import it.unibo.t2sgame.game.model.api.World;
+
 /**
- * This interface abstracts the concepts of "Game" ("Partita" in italian)
+ * This interface abstracts the concepts of "Game" ("Partita" in italian).
  */
 public interface Game {
     /**
      * 
-     * @return the state of the current game
+     * @return the state of the current game.
      */
     State getState();
+
     /**
      * 
-     * @return the world of the current game
+     * @return the world of the current game.
      */
     World getWorld();
+
     /**
      * 
      * @return true if the logics of the game determs the end of the game,
-     * otherwise false
+     *         otherwise false.
      */
     boolean isOver();
+
     /**
-     * Update the game based on the events and checks occoured
+     * Update the game based on the events and checks occoured.
      */
     void update();
 }
