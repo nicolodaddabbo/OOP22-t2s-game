7998a3290a22ab1b69e21ebd925f79896cb94384
Luca Oskari Fiumano <lucaoskari.fiumano@studio.unibo.it>
2023-02-09 11:29:05 +0100

    [TEST] changed test for HealthComponent and for PowerUp

--- src/test/java/it/unibo/t2sgame/model/HealthComponentTest.java
+++ src/test/java/it/unibo/t2sgame/model/HealthComponentTest.java
@@ -20,17 +20,8 @@ public class HealthComponentTest {
     }
 
     @Test
-    public void testStartHealth() {
-        var entity = createBaseEntity();
-        assertTrue(entity.getComponent(HealthComponent.class).isPresent());
-        assertEquals(this.startingHealth, entity.getComponent(HealthComponent.class).get().getHealth());
-    }
-
-    @Test
     public void testDamageReceived() {
         var entity = createBaseEntity();
-        assertTrue(entity.getComponent(HealthComponent.class).isPresent());
-        assertEquals(this.startingHealth, entity.getComponent(HealthComponent.class).get().getHealth());
         entity.notifyComponent(HealthComponent.class, () -> this.damage);
         assertEquals(this.startingHealth - this.damage, entity.getComponent(HealthComponent.class).get().getHealth());
     }
@@ -38,8 +29,6 @@ public class HealthComponentTest {
     @Test
     public void testBehaviourWhenDamageIsBiggerThanCurrentHealth(){
         var entity = createBaseEntity();
-        assertTrue(entity.getComponent(HealthComponent.class).isPresent());
-        assertEquals(this.startingHealth, entity.getComponent(HealthComponent.class).get().getHealth());
         entity.notifyComponent(HealthComponent.class, () -> this.startingHealth*2);
         assertEquals(0, entity.getComponent(HealthComponent.class).get().getHealth());
     }
--- src/test/java/it/unibo/t2sgame/model/PowerUpTest.java
+++ src/test/java/it/unibo/t2sgame/model/PowerUpTest.java
@@ -3,81 +3,81 @@ package it.unibo.t2sgame.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import it.unibo.t2sgame.common.Vector2D;
 import it.unibo.t2sgame.core.components.impl.PhysicsComponent;
 import it.unibo.t2sgame.core.entity.api.Entity;
+import it.unibo.t2sgame.core.entity.api.Type;
+import it.unibo.t2sgame.core.entity.impl.EntityImpl;
 import it.unibo.t2sgame.game.components.HealthComponent;
 import it.unibo.t2sgame.game.components.ShootComponent;
 import it.unibo.t2sgame.game.model.api.PowerUpFactory;
-import it.unibo.t2sgame.game.model.impl.EntityFactoryImpl;
 import it.unibo.t2sgame.game.model.impl.PowerUpFactoryImpl;
 
 public class PowerUpTest {
     private PowerUpFactory powerUpFactory = new PowerUpFactoryImpl();
+    private static final double FIRERATE_SECONDS = 1.5;
+    private static final double PROJECTILE_SPEED = 1.0;
+    private static final int PROJECTILE_DAMAGE = 1;
+    private static final double PROJECTILE_SIZE = 20;
+    private static final int HEALTH = 3;
+    private static final double SPEED = 1;
 
     private Entity createBasePlayerEntity() {
-        return new EntityFactoryImpl().createPlayer(new Vector2D(0, 0));
+        return new EntityImpl(new Vector2D(0, 0), Type.PLAYER)
+        .addComponent(new PhysicsComponent(SPEED))
+        .addComponent(new HealthComponent(HEALTH))
+        .addComponent(new ShootComponent(FIRERATE_SECONDS, PROJECTILE_SPEED, PROJECTILE_DAMAGE, PROJECTILE_SIZE));
     }
 
     @Test
     void testHealthUpPowerUp() {
         var entity = this.createBasePlayerEntity();
         var healthComponent = entity.getComponent(HealthComponent.class);
-        assertTrue(healthComponent.isPresent());
-        assertEquals(3, healthComponent.get().getHealth());
         powerUpFactory.generateHealthUpPowerUp().obtain(entity);
-        assertEquals(4, healthComponent.get().getHealth());
+        assertEquals(HEALTH+1, healthComponent.get().getHealth());
     }
 
     @Test
     void testDamageBoostPowerUp() {
         var entity = this.createBasePlayerEntity();
         var shootComponent = entity.getComponent(ShootComponent.class);
-        assertTrue(shootComponent.isPresent());
-        assertEquals(1, shootComponent.get().getProjectileDamage());
         powerUpFactory.generateDamageBoostPowerUp().obtain(entity);
-        assertEquals(2, shootComponent.get().getProjectileDamage());
+        assertEquals(PROJECTILE_DAMAGE+1, shootComponent.get().getProjectileDamage());
     }
 
     @Test
     void testFireRatioPowerUp() {
         var entity = this.createBasePlayerEntity();
         var shootComponent = entity.getComponent(ShootComponent.class);
-        assertTrue(shootComponent.isPresent());
-        assertEquals(0.5, shootComponent.get().getfireRateSeconds());
         powerUpFactory.generateFireRatioPowerUp().obtain(entity);
-        assertEquals(0.45, shootComponent.get().getfireRateSeconds());
+        assertEquals(FIRERATE_SECONDS-0.05, shootComponent.get().getfireRateSeconds());
     }
 
     @Test
     void testSpeedUpPowerUp() {
         var entity = this.createBasePlayerEntity();
         var physicsComponent = entity.getComponent(PhysicsComponent.class);
-        assertTrue(physicsComponent.isPresent());
-        assertEquals(1, physicsComponent.get().getSpeed());
         powerUpFactory.generateSpeedUpPowerUp().obtain(entity);
-        assertEquals(1.25, physicsComponent.get().getSpeed());
+        assertEquals(SPEED+0.25, physicsComponent.get().getSpeed());
     }
 
     @Test
     void testProjectileSpeedUp() {
         var entity = this.createBasePlayerEntity();
         var shootComponent = entity.getComponent(ShootComponent.class);
-        assertTrue(shootComponent.isPresent());
-        assertEquals(1.5, shootComponent.get().getProjectileSpeed());
         powerUpFactory.generateProjectileSpeedUpPowerUp().obtain(entity);
-        assertEquals(2.5, shootComponent.get().getProjectileSpeed());
+        assertEquals(PROJECTILE_SPEED+1, shootComponent.get().getProjectileSpeed());
     }
 
     @Test
     void testProjectileSizeUp() {
         var entity = this.createBasePlayerEntity();
         var shootComponent = entity.getComponent(ShootComponent.class);
-        assertTrue(shootComponent.isPresent());
-        assertEquals(20.0, shootComponent.get().getProjectileSize());
         powerUpFactory.generateProjectileSizeUpPowerUp().obtain(entity);
-        assertEquals(25.0, shootComponent.get().getProjectileSize());
+        assertEquals(PROJECTILE_SIZE+5, shootComponent.get().getProjectileSize());
     }
 }
