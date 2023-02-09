a68e578069773bd90a9af7bc0142c28ec85322b0
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-09 18:58:32 +0100

    [DOCS] Added docs in Type enum

--- src/main/java/it/unibo/t2sgame/core/entity/api/Type.java
+++ src/main/java/it/unibo/t2sgame/core/entity/api/Type.java
@@ -1,9 +1,27 @@
 package it.unibo.t2sgame.core.entity.api;
 
+/**
+ * This enum represents the types of the entities.
+ */
 public enum Type {
+    /**
+     * Player type.
+     */
     PLAYER, 
+    /**
+     * Companion type.
+     */
     COMPANION,
+    /**
+     * Enemy type.
+     */
     ENEMY, 
+    /**
+     * Projectile type.
+     */
     PROJECTILE, 
+    /**
+     * Wall type.
+     */
     WALL
 }
