0c260f0c7184ab2f7ebe1f150f0bb93cc3f38a3b
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-09 11:42:51 +0100

    [STYLE] Made equals method parameter final in Vector2D

--- src/main/java/it/unibo/t2sgame/common/Vector2D.java
+++ src/main/java/it/unibo/t2sgame/common/Vector2D.java
@@ -138,7 +138,7 @@ public class Vector2D {
      * {@inheritDoc}
      */
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(final Object obj) {
         if (this == obj) {
             return true;
         }
