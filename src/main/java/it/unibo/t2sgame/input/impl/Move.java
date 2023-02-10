cebf331cf957f40e373873c924cb97296092d5b5
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-10 01:33:30 +0100

    [FIX] Overridden hashCode and equals methods

--- src/main/java/it/unibo/t2sgame/input/impl/Move.java
+++ src/main/java/it/unibo/t2sgame/input/impl/Move.java
@@ -26,4 +26,37 @@ public class Move implements Command {
     public void execute(final Entity entity) {
         entity.notifyComponent(PhysicsComponent.class, () -> this.direction);
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((direction == null) ? 0 : direction.hashCode());
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Move other = (Move) obj;
+        if (direction != other.direction) {
+            return false;
+        }
+        return true;
+    }
+
 }
