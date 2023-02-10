e17529d883c86f6abfd5ef6a09e502a376bde732
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-10 01:58:42 +0100

    [TEST] Fixed code style errors

--- src/test/java/it/unibo/t2sgame/model/InputTest.java
+++ src/test/java/it/unibo/t2sgame/model/InputTest.java
@@ -24,14 +24,22 @@ import it.unibo.t2sgame.input.impl.KeyboardInputController;
 import it.unibo.t2sgame.input.impl.Move;
 import it.unibo.t2sgame.input.impl.Shoot;
 
+/**
+ * Class that contains input related tests.
+ */
 public class InputTest {
-    ComponentFactory componentFactory = new ComponentFactoryImpl();
-    EntityFactory entityFactory = new EntityFactoryImpl();
-    WorldFactory worldFactory = new WorldFactoryImpl();
-    World world;
-    Entity player;
-    Entity enemy;
-    
+    private static final int MOVE_UP_CODE = 38;
+    private static final int SHOOT_UP_CODE = 87;
+    private final ComponentFactory componentFactory = new ComponentFactoryImpl();
+    private final EntityFactory entityFactory = new EntityFactoryImpl();
+    private final WorldFactory worldFactory = new WorldFactoryImpl();
+    private final World world;
+    private final Entity player;
+    private final Entity enemy;
+
+    /**
+     * Initialize the world, player and enemy fields.
+     */
     public InputTest() {
         this.world = this.worldFactory.createWorldWithOnePlayer();
         this.player = this.world.getPlayers().get(0);
@@ -42,33 +50,40 @@ public class InputTest {
 
     @Test
     void keyboardInputTest() {
-        InputComponent keyboardInputComponent = (InputComponent) this.componentFactory.createInputComponentFrom(new KeyboardInputController());
+        final InputComponent keyboardInputComponent = (InputComponent) this.componentFactory
+                .createInputComponentFrom(new KeyboardInputController());
         // add the input component to the player
         this.player.addComponent(keyboardInputComponent);
-        KeyboardInputController inputController = (KeyboardInputController) keyboardInputComponent.getInputController();
+        final KeyboardInputController inputController = (KeyboardInputController) keyboardInputComponent
+                .getInputController();
         // with no key pressed the commands queue should be empty
         assertTrue(inputController.getCommandsQueue().isEmpty());
         // 'Arrow UP' key is pressed
-        inputController.notifyKeyPressed(38);
+        inputController.notifyKeyPressed(MOVE_UP_CODE);
         // command should be a Shoot command
         assertEquals(Shoot.class, inputController.getCommandsQueue().poll().getClass());
         // 'W' key is pressed
-        inputController.notifyKeyPressed(87);
-        // command should be a Move command, and the previous Shoot command shouldn't exists
+        inputController.notifyKeyPressed(SHOOT_UP_CODE);
+        // command should be a Move command, and the previous Shoot command shouldn't
+        // exists
         assertEquals(Move.class, inputController.getCommandsQueue().poll().getClass());
     }
 
     @Test
     void chasingAIInputTest() {
-        InputComponent aiInputComponent = (InputComponent) this.componentFactory.createInputComponentFrom(new ChasingAIInputController(Type.PLAYER));
+        final InputComponent aiInputComponent = (InputComponent) this.componentFactory
+                .createInputComponentFrom(new ChasingAIInputController(Type.PLAYER));
         this.enemy.addComponent(aiInputComponent);
-        ChasingAIInputController inputController = (ChasingAIInputController) aiInputComponent.getInputController();
-        // every time the getCommandsQueue method gets called the AI generate a command, so the
+        final ChasingAIInputController inputController = (ChasingAIInputController) aiInputComponent
+                .getInputController();
+        // every time the getCommandsQueue method gets called the AI generate a command,
+        // so the
         // commands queue should never be empty
         assertFalse(inputController.getCommandsQueue().isEmpty());
         aiInputComponent.update();
         this.enemy.getComponent(PhysicsComponent.class).get().update();
-        // a ChasingAIInputController generates only Move commands, so after an update the position
+        // a ChasingAIInputController generates only Move commands, so after an update
+        // the position
         // of the player should be different from the initial one
         assertNotEquals(this.enemy.getPosition(), new Vector2D(0, 0));
     }
